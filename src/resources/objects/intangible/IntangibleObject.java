68b15090ca58ea933a78c210cfc23c770a65bf29
Obique <obique.pswg@gmail.com>
2014-12-06 00:54:32 -0600

    Added baselines for IntangibleObject

--- src/resources/objects/intangible/IntangibleObject.java
+++ src/resources/objects/intangible/IntangibleObject.java
@@ -1,11 +1,23 @@
 package resources.objects.intangible;
 
+import resources.network.BaselineBuilder;
 import resources.objects.SWGObject;
+import resources.player.Player;
 
 public class IntangibleObject extends SWGObject {
 	
+	private int	count	= 0;
+	
 	public IntangibleObject(long objectId) {
 		super(objectId);
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public void setCount(int count) {
+		this.count = count;
 	}
 	
 }
