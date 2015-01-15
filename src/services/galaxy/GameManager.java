3f15a546862c3485f7346b2f3942a9820d0f2963
Waverunner <lewaverunner@gmail.com>
2015-01-15 11:38:01 -0500

    Added SuiService to GameManager

--- src/services/galaxy/GameManager.java
+++ src/services/galaxy/GameManager.java
@@ -3,20 +3,24 @@ package services.galaxy;
 import resources.control.Manager;
 import services.chat.ChatService;
 import services.commands.CommandService;
+import services.sui.SuiService;
 
 public class GameManager extends Manager {
 	
 	private ChatService chatService;
 	private CommandService commandService;
 	private ConnectionService connectionService;
+	private SuiService suiService;
 	
 	public GameManager() {
 		chatService = new ChatService();
 		commandService = new CommandService();
 		connectionService = new ConnectionService();
+		suiService = new SuiService();
 		
 		addChildService(chatService);
 		addChildService(commandService);
 		addChildService(connectionService);
+		addChildService(suiService);
 	}
 }
