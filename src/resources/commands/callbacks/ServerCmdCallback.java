2fc1e3e8c007e38f3a6210ba9e1d8300c4ce3d6e
Waverunner <lewaverunner@gmail.com>
2015-01-18 10:53:49 -0500

    Added handling for listbox selection on /server command

--- src/resources/commands/callbacks/ServerCmdCallback.java
+++ src/resources/commands/callbacks/ServerCmdCallback.java
@@ -6,6 +6,8 @@ import resources.commands.ICmdCallback;
 import resources.objects.SWGObject;
 import resources.player.Player;
 import resources.sui.ISuiCallback;
+import resources.sui.SuiInputBox;
+import resources.sui.SuiInputBox.InputBoxType;
 import resources.sui.SuiListBox;
 import resources.sui.SuiListBox.ListBoxType;
 import services.objects.ObjectManager;
@@ -29,8 +31,43 @@ public class ServerCmdCallback implements ICmdCallback {
 
 		@Override
 		public void handleEvent(Player player, SWGObject actor, int eventType, List<String> returnParams) {
-
+			int selection = SuiListBox.getSelectedIndex(returnParams);
+			
+			switch(selection) {
+			case 0: handleShutdownServer(); break;
+			case 1: handleKickPlayer(player); break;
+			case 2: handleBanPlayer(player); break;
+			default: break;
+			}
 		}
 		
+		private void handleShutdownServer() {
+			// TODO: initiate server shutdown
+		}
+		
+		private void handleKickPlayer(Player actor) {
+			SuiInputBox window = new SuiInputBox(actor, InputBoxType.OK_CANCEL, "Kick Player", "Enter the name of the player that you wish to KICK from the server.");
+			window.addInputTextCallback(0, new ISuiCallback() {
+				@Override
+				public void handleEvent(Player player, SWGObject actor, int eventType, List<String> returnParams) {
+					//String name = returnParams.get(0); // input
+					// TODO: Kick the player that has name entered
+				}
+				
+			});
+			window.display();
+		}
+		
+		private void handleBanPlayer(Player actor) {
+			SuiInputBox window = new SuiInputBox(actor, InputBoxType.OK_CANCEL, "Ban Player", "Enter the name of the player that you wish to BAN from the server.");
+			window.addInputTextCallback(0, new ISuiCallback() {
+				@Override
+				public void handleEvent(Player player, SWGObject actor, int eventType, List<String> returnParams) {
+					//String name = returnParams.get(0); // input
+					// TODO: Ban the player that has name entered
+				}
+			});
+			window.display();
+		}
 	}
 }
