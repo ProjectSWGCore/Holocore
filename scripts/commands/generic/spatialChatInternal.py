512c32a36134264d7030bbca6ea17acb8217ddfc
Waverunner <lewaverunner@gmail.com>
2015-01-18 09:29:17 -0500

    Fixed argument error for spatialChatInternal command

--- scripts/commands/generic/spatialChatInternal.py
+++ scripts/commands/generic/spatialChatInternal.py
@@ -5,6 +5,5 @@ import sys
 def execute(objManager, player, target, args):
 	cmdArgs = args.split(' ', 5)
 	#print ('unkInt=' + cmdArgs[0] + ' chatType=' + cmdArgs[1] + ' moodId=' + cmdArgs[2] + ' unkInt2=' + cmdArgs[3] + ' langId=' + cmdArgs[4])
-
 	SpatialChatIntent(player, int(cmdArgs[1]), cmdArgs[5], int(cmdArgs[2])).broadcast()
 	return
