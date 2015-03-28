119810419250075b0c5ef1773944d07980d045fc
wallaceg09 <wallaceg09@yahoo.com>
2015-03-28 04:56:50 -0500

    Fixed a bug where kneeling or sitting before going prone wouldn't allow the player to turn/move left or right until they stood and went prone again.

--- src/resources/commands/callbacks/ProneCmdCallback.java
+++ src/resources/commands/callbacks/ProneCmdCallback.java
@@ -14,6 +14,7 @@ public class ProneCmdCallback implements ICmdCallback {
 	public void execute(GalacticManager galacticManager, Player player, SWGObject target, String args) {
 		CreatureObject creature = player.getCreatureObject(); 
 		creature.setPosture(Posture.PRONE);
+		creature.setTurnScale(1);
 		creature.setMovementScale(0.50);
 		creature.sendObservers(new PostureUpdate(creature.getObjectId(), Posture.PRONE));
 		
