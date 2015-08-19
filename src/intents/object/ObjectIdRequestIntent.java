f779c3f302cedbe7ffe98ae624a59e4a832a23e6
Waverunner <lewaverunner@gmail.com>
2015-08-19 11:21:24 -0400

    Added ability for services to reserve X amount of object id's in the ObjectManager through ObjectIdResponseIntent which will respond with a ObjectIdResponseIntent with a list of the id's that have been reserved Added ObjectCreateIntent which will add an object to the object map in ObjectManager and the awareness system if required

---
+++ src/intents/object/ObjectIdRequestIntent.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2015 /// Project SWG /// www.projectswg.com
+ *
+ * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on
+ * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies.
+ * Our goal is to create an emulator which will provide a server for players to
+ * continue playing a game similar to the one they used to play. We are basing
+ * it on the final publish of the game prior to end-game events.
+ *
+ * This file is part of Holocore.
+ *
+ * --------------------------------------------------------------------------------
+ *
+ * Holocore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Holocore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+package intents.object;
+
+import resources.control.Intent;
+
+/**
+ * Created by Waverunner on 8/19/2015
+ */
+public class ObjectIdRequestIntent extends Intent {
+	public static final String TYPE = "ObjectIdRequestIntent";
+
+	private String identifier;
+	private int amount;
+
+	public ObjectIdRequestIntent(String identifier, int amount) {
+		super(TYPE);
+		this.identifier = identifier;
+		this.amount = amount;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+}
