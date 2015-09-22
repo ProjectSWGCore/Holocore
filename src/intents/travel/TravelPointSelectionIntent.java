a3df367d309a08e5af0ccea850651d6911db8d96
Mads Boddum <danish_cyborg@hotmail.com>
2015-09-22 16:22:53 +0200

    Added a instant field to TravelPointSelectionIntent for use with ITVs

--- src/intents/travel/TravelPointSelectionIntent.java
+++ src/intents/travel/TravelPointSelectionIntent.java
@@ -8,14 +8,20 @@ public class TravelPointSelectionIntent extends Intent {
 	public static final String TYPE = "TravelPointSelectionIntent";
 	
 	private CreatureObject creature;
+	private boolean instant;
 	
-	public TravelPointSelectionIntent(CreatureObject creature) {
+	public TravelPointSelectionIntent(CreatureObject creature, boolean instant) {
 		super(TYPE);
 		this.creature = creature;
+		this.instant = instant;
 	}
 	
 	public CreatureObject getCreature() {
 		return creature;
+	}
+	
+	public boolean isInstant() {
+		return instant;
 	}
 	
 }
