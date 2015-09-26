a88b58905cb3845aa5081d49657cd7234682f8e2
Mads Boddum <danish_cyborg@hotmail.com>
2015-09-26 07:10:39 +0200

    Clicking a Ticket Terminal now opens the appropriate window for purchasing tickets

--- scripts/radial/terminal/travel.js
+++ scripts/radial/terminal/travel.js
@@ -7,6 +7,8 @@ function handleSelection(player, target, selection) {
 	switch (selection) {
 		case RadialItem.ITEM_USE:
 			Log.d("travel.js", "Travel Selection: ITEM_USE");
+			var TravelPointSelectionIntent = Java.type("intents.travel.TravelPointSelectionIntent");
+			new TravelPointSelectionIntent(player.getCreatureObject(), false).broadcast();
 			break;
 	}
 };
