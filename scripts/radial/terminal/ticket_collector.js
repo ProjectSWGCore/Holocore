b4bca91cd4a8f2c14b17a17cc58fdef545c228c1
Mads Boddum <danish_cyborg@hotmail.com>
2015-09-26 07:08:31 +0200

    The Ticket Collector now broadcasts a TicketUseIntent

--- scripts/radial/terminal/ticket_collector.js
+++ scripts/radial/terminal/ticket_collector.js
@@ -1,12 +1,16 @@
 function getOptions(options) {
 	options.add(new RadialOption(RadialItem.ITEM_USE));
 	options.add(new RadialOption(RadialItem.EXAMINE));
-}
+};
 
 function handleSelection(player, target, selection) {
 	switch (selection) {
 		case RadialItem.ITEM_USE:
 			Log.d("ticket_collector.js", "Ticket Collector Selection: ITEM_USE");
+			var TicketUseIntent = Java.type("intents.travel.TicketUseIntent");
+			
+			new TicketUseIntent(player).broadcast();
+			
 			break;
 	}
 };
