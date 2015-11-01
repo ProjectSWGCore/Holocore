e23512f83b0e6ec8b92ad950c74018a5cd8d7645
Obique PSWG <obique.pswg@gmail.com>
2015-11-01 08:59:02 -0600

    Added string to byte array function to ByteUtilities

--- src/utilities/ByteUtilities.java
+++ src/utilities/ByteUtilities.java
@@ -48,6 +48,17 @@ public class ByteUtilities {
 		return new String(data);
 	}
 	
+	public static byte [] getHexStringArray(String string) {
+		int len = string.length();
+		if (len % 2 != 0)
+			return new byte[0];
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(string.charAt(i), 16) << 4) + Character.digit(string.charAt(i+1), 16));
+		}
+		return data;
+	}
+	
 	public static byte [] longToBytes(long l) {
 		byte [] b = new byte[Long.SIZE];
 		synchronized (buffer) {
