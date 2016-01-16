286ed21843a0acb8ccfe8204e0ba2a3646e08f49
Obique PSWG <obique.pswg@gmail.com>
2016-01-16 03:27:42 -0600

    Implemented basic NPC spawning

--- src/services/spawn/SpawnerService.java
+++ src/services/spawn/SpawnerService.java
@@ -27,6 +27,9 @@
 ***********************************************************************************/
 package services.spawn;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.NoSuchFileException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -40,6 +43,8 @@ import resources.control.Intent;
 import resources.control.Service;
 import resources.objects.building.BuildingObject;
 import resources.objects.SWGObject;
+import resources.server_info.Log;
+import resources.server_info.RelationalDatabase;
 import resources.server_info.RelationalServerData;
 import resources.server_info.RelationalServerFactory;
 import resources.spawn.SpawnerType;
@@ -48,26 +53,24 @@ import services.objects.ObjectManager;
 
 public final class SpawnerService extends Service {
 	
-	private static final String GET_ALL_SPAWNERS_SQL = "SELECT static.*, buildings.object_id, buildings.terrain_name FROM static, buildings WHERE buildings.building_id = static.building_id";
+	private static final String GET_ALL_SPAWNERS_SQL = "SELECT static.*, buildings.object_id, buildings.terrain_name, creatures.iff_template "
+			+ "FROM static, buildings, creatures "
+			+ "WHERE buildings.building_id = static.building_id AND static.creature_id = creatures.creature_id";
 	
 	private final ObjectManager objectManager;
 	private final Collection<Spawner> spawners;
-	private final RelationalServerData spawnerDatabase;
 	
 	public SpawnerService(ObjectManager objectManager) {
 		this.objectManager = objectManager;
 		spawners = new ArrayList<>();
-		spawnerDatabase = RelationalServerFactory.getServerData("spawn/static.db", "static", "building/buildings");
-		if (spawnerDatabase == null)
-			throw new main.ProjectSWG.CoreException("Unable to load sdb files for SpawnerService");
 		
 		registerForIntent(ConfigChangedIntent.TYPE);
 	}
 	
 	@Override
 	public boolean initialize() {
-		if (getConfig(ConfigFile.FEATURES).getBoolean("SPAWNERS-ENABLED", false))
-			loadSpawners();
+		if (getConfig(ConfigFile.FEATURES).getBoolean("NPCS-ENABLED", true))
+			loadSpawners(getConfig(ConfigFile.FEATURES).getBoolean("SPAWN-EGGS-ENABLED", false));
 		
 		return super.initialize();
 	}
@@ -80,13 +83,13 @@ public final class SpawnerService extends Service {
 		String newValue, oldValue;
 		
 		if(cgi.getChangedConfig().equals(ConfigFile.FEATURES))
-			if(cgi.getKey().equals("SPAWNERS-ENABLED")) {
+			if(cgi.getKey().equals("NPCS-ENABLED")) {
 				newValue = cgi.getNewValue();
 				oldValue = cgi.getOldValue();
 				
 				if(!newValue.equals(oldValue)) {
 					if(Boolean.valueOf(newValue) && spawners.isEmpty()) { // If nothing's been spawned, create it.
-						loadSpawners();
+						loadSpawners(getConfig(ConfigFile.FEATURES).getBoolean("SPAWN-EGGS-ENABLED", false));
 					} else { // If anything's been spawned, delete it.
 						removeSpawners();
 					}
@@ -95,36 +98,55 @@ public final class SpawnerService extends Service {
 		
 	}
 	
-	@Override
-	public boolean terminate() {
-		spawnerDatabase.close();
-		return super.terminate();
-	}
-	
-	private void loadSpawners() {
-		try (ResultSet jointTable = spawnerDatabase.prepareStatement(GET_ALL_SPAWNERS_SQL).executeQuery()) {
-			while (jointTable.next()) {
-				if (jointTable.getBoolean("active")) {
-					Location loc = new Location(jointTable.getFloat("x"), jointTable.getFloat("y"), jointTable.getFloat("z"), Terrain.valueOf(jointTable.getString("terrain_name")));
-					SpawnerType spawnerType = SpawnerType.valueOf(jointTable.getString("spawner_type"));
-					long objectId = jointTable.getLong("object_id");
-					int cellId = jointTable.getInt("cell_id");
-					loc.setOrientation(jointTable.getFloat("oX"), jointTable.getFloat("oY"), jointTable.getFloat("oZ"), jointTable.getFloat("oW"));
-					
-					SWGObject parent = null;
-					if (cellId > 0) {
-						parent = objectManager.getObjectById(objectId);
-						if (parent instanceof BuildingObject)
-							parent = ((BuildingObject) parent).getCellByNumber(cellId);
+	private void loadSpawners(boolean spawnEggs) {
+		try (RelationalDatabase spawnerDatabase = RelationalServerFactory.getServerData("spawn/static.db", "static", "building/buildings", "creatures/creatures")) {
+			try (ResultSet jointTable = spawnerDatabase.executeQuery(GET_ALL_SPAWNERS_SQL)) {
+				while (jointTable.next()) {
+					if (jointTable.getBoolean("active")) {
+						loadSpawner(jointTable, spawnEggs);
 					}
-					SWGObject egg = objectManager.createObject(parent, spawnerType.getObjectTemplate(), loc, false);
-					
-					spawners.add(new Spawner(egg));
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void loadSpawner(ResultSet set, boolean spawnEggs) throws SQLException {
+		Location loc = new Location(set.getFloat("x"), set.getFloat("y"), set.getFloat("z"), Terrain.valueOf(set.getString("terrain_name")));
+		SpawnerType spawnerType = SpawnerType.valueOf(set.getString("spawner_type"));
+		long objectId = set.getLong("object_id");
+		int cellId = set.getInt("cell_id");
+		loc.setOrientation(set.getFloat("oX"), set.getFloat("oY"), set.getFloat("oZ"), set.getFloat("oW"));
+		
+		SWGObject parent = null;
+		if (cellId > 0) {
+			parent = objectManager.getObjectById(objectId);
+			if (parent instanceof BuildingObject)
+				parent = ((BuildingObject) parent).getCellByNumber(cellId);
+		}
+		
+		if (spawnEggs) {
+			SWGObject egg = objectManager.createObject(parent, spawnerType.getObjectTemplate(), loc, false);
+			spawners.add(new Spawner(egg));
+		}
+		createNPC(parent, loc, set.getString("iff_template"), set.getString("creature_name"));
+	}
+	
+	private void createNPC(SWGObject parent, Location loc, String iff, String name) {
+		String [] possible = iff.split(";");
+		String template = possible[(int) (Math.random()*possible.length)];
+		if (template.contains("/")) {
+			int ind = template.lastIndexOf('/');
+			template = "object/mobile/" + template.substring(0, ind) + "/shared_" + template.substring(ind+1);
+		} else
+			template = "object/mobile/shared_" + template;
+		File f = new File("clientdata/"+template);
+		if (f.isFile()) {
+			SWGObject object = objectManager.createObject(parent, template, loc, false);
+			object.setName(name);
+		} else
+			Log.e(this, "Unknown template: " + template);
 	}
 	
 	private void removeSpawners() {
