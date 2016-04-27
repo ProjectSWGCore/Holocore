caea959bec95b58d4cd2a19a90f4c752cab85c90
Obique PSWG <obique.pswg@gmail.com>
2016-04-27 12:28:17 -0500

    Implemented the LagRequest packet/response

--- src/network/PacketType.java
+++ src/network/PacketType.java
@@ -61,6 +61,7 @@ public enum PacketType {
 	SERVER_UNIX_EPOCH_TIME						(ServerUnixEpochTime.CRC, 	ServerUnixEpochTime.class),
 	SERVER_ID									(ServerId.CRC, 				ServerId.class),
 	SERVER_STRING								(ServerString.CRC, 			ServerString.class),
+	LAG_REQUEST									(LagRequest.CRC,			LagRequest.class),
 
 	// Login
 	CLIENT_ID_MSG								(ClientIdMsg.CRC, 				ClientIdMsg.class),
@@ -70,6 +71,7 @@ public enum PacketType {
 	REQUEST_EXTENDED_CLUSTERS					(RequestExtendedClusters.CRC, 	RequestExtendedClusters.class),
 	OFFLINE_SERVERS_MESSAGE     				(OfflineServersMessage.CRC, 	OfflineServersMessage.class),
 	SERVER_NOW_EPOCH_TIME						(ServerNowEpochTime.CRC,		ServerNowEpochTime.class),
+	CONNECTION_SERVER_LAG_RESPONSE				(ConnectionServerLagResponse.CRC,ConnectionServerLagResponse.class),
 
 		// Post-Login
 		LOGIN_CLIENT_ID							(LoginClientId.CRC, 			LoginClientId.class),
@@ -95,6 +97,7 @@ public enum PacketType {
 		DELETE_CHARACTER_REQUEST				(DeleteCharacterRequest.CRC, 	DeleteCharacterRequest.class),
 
 	// Zone
+	GAME_SERVER_LAG_RESPONSE					(GameServerLagResponse.CRC,			GameServerLagResponse.class),
 	COMMAND_QUEUE_ENQUEUE						(0x00000116, 						CommandQueueEnqueue.class),
 	SELECT_CHARACTER							(SelectCharacter.CRC, 				SelectCharacter.class),
 	CMD_SCENE_READY								(CmdSceneReady.CRC, 				CmdSceneReady.class),
