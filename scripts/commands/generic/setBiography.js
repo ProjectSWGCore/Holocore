472d8a460da833e6609886b154183502c4b04605
Mads Boddum <danish_cyborg@hotmail.com>
2016-05-15 14:25:18 +0200

    Fixed an issue where the biography wouldn't be saved once set

--- scripts/commands/generic/setBiography.js
+++ scripts/commands/generic/setBiography.js
@@ -1,6 +1,8 @@
 function executeCommand(galacticManager, player, target, args) {
 	var BiographyUpdate = Java.type("network.packets.swg.zone.object_controller.BiographyUpdate");
-	var objectId = player.getCreatureObject().getObjectId();
+	var creatureObject = player.getCreatureObject();
+	var objectId = creatureObject.getObjectId();
 	
+	creatureObject.getPlayerObject().setBiography(args);
 	player.sendPacket(new BiographyUpdate(objectId, objectId, args));
 }
