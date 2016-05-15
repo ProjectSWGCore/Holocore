79952582311df76d256a8decfffcaa6605a94776
Mads Boddum <danish_cyborg@hotmail.com>
2016-05-15 15:23:50 +0200

    Added a 1025 character limit for biographies

--- scripts/commands/generic/setBiography.js
+++ scripts/commands/generic/setBiography.js
@@ -1,4 +1,9 @@
 function executeCommand(galacticManager, player, target, args) {
+	
+	if(args.length() > 1025) {
+		return;
+	}
+	
 	var BiographyUpdate = Java.type("network.packets.swg.zone.object_controller.BiographyUpdate");
 	var creatureObject = player.getCreatureObject();
 	var objectId = creatureObject.getObjectId();
