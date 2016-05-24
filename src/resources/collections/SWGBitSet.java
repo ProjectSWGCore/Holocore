d8b22aa6ec8b48deeb571ad57f2c3fd4c866eb5a
Obique PSWG <obique.pswg@gmail.com>
2016-05-24 09:35:26 -0500

    Created test cases for SWGBitSet and SWGFlag

--- src/resources/collections/SWGBitSet.java
+++ src/resources/collections/SWGBitSet.java
@@ -8,19 +8,18 @@ import java.nio.ByteOrder;
 import java.util.BitSet;
 
 public class SWGBitSet extends BitSet implements Encodable {
-
+	
 	private static final long serialVersionUID = 1L;
-		
+	
 	private int view;
 	private int updateType;
-
-
+	
 	public SWGBitSet(int view, int updateType) {
 		super(128); // Seems to be the default size for the bitmask sets in packets
 		this.view = view;
 		this.updateType = updateType;
 	}
-
+	
 	@Override
 	public byte[] encode() {
 		BitSet b = ((BitSet) this);
@@ -28,24 +27,18 @@ public class SWGBitSet extends BitSet implements Encodable {
 		ByteBuffer buffer = ByteBuffer.allocate(8 + bytes.length).order(ByteOrder.LITTLE_ENDIAN);
 		buffer.putInt(bytes.length);
 		buffer.putInt(b.length());
-		buffer.put(bytes).array();
+		buffer.put(bytes);
 		return buffer.array();
 	}
-
+	
 	@Override
 	public void decode(ByteBuffer data) {
 		// TODO: Decode method for SWGBitSet
 		throw new UnsupportedOperationException("Unable to decode bitset!");
 	}
-		
+	
 	public void sendDeltaMessage(SWGObject target) {
-		if (target.getOwner() == null) {
-			return;
-		}
-
 		target.sendDelta(view, updateType, encode());
 	}
-
-}	
 	
-
+}
