d473a5c729c57fccf93aea8091b25f94e7969a5e
Mads Boddum <danish_cyborg@hotmail.com>
2016-09-18 08:11:02 +0200

    Fixed lingering thread in TCPServer

--- src/resources/network/TCPServer.java
+++ src/resources/network/TCPServer.java
@@ -33,6 +33,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedByInterruptException;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
@@ -126,6 +127,7 @@ public class TCPServer {
 	
 	public boolean close() {
 		listener.stop();
+		callbackExecutor.shutdown();
 		try {
 			if (channel != null)
 				channel.close();
@@ -267,6 +269,10 @@ public class TCPServer {
 						callbackExecutor.execute(() -> callback.onIncomingData(s.socket(), smaller.array()));
 					return true;
 				}
+			} catch (ClosedByInterruptException e) {
+				key.cancel();
+				disconnect(s);
+				stop();
 			} catch (IOException e) {
 				if (e.getMessage() != null && e.getMessage().toLowerCase(Locale.US).contains("connection reset"))
 					Log.e("TCPServer", "Connection Reset with %s", s.socket().getRemoteSocketAddress());
