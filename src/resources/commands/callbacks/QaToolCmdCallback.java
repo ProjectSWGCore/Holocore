aad9062a510417ecd92deada495d727f47bfe53b
Obique PSWG <obique.pswg@gmail.com>
2016-11-24 23:34:36 -0600

    Fixed qatool recover

--- src/resources/commands/callbacks/QaToolCmdCallback.java
+++ src/resources/commands/callbacks/QaToolCmdCallback.java
@@ -173,8 +173,11 @@ public class QaToolCmdCallback implements ICmdCallback {
 		String name = args;
 		String[] nameParts = name.split(" ");
 		String loc = "";
-		if (nameParts.length >= 2) {
+		if (nameParts.length == 2) {
 			name = nameParts[0];
+			loc = nameParts[1];
+		} else if (nameParts.length == 3) {
+			name = nameParts[0] + " " + nameParts[1];
 			loc = nameParts[1];
 		} else {
 			sendSystemMessage(player, "Invalid arguments! Expected <playername> [opt]<terrain>");
@@ -184,7 +187,7 @@ public class QaToolCmdCallback implements ICmdCallback {
 	}
 	
 	private void recoverPlayer(ObjectManager objManager, PlayerManager playerManager, Player player, String name, String loc) {
-		long id = playerManager.getPlayerByCreatureFirstName(name).getCreatureObject().getObjectId();
+		long id = playerManager.getCharacterIdByName(name);
 		if (id == 0) {
 			sendSystemMessage(player, "Could not find player by name: '" + name + "'");
 			return;
