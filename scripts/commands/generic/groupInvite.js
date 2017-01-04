0ef1976fc53693d361d92033bf0e57287fa9d189
Mads Boddum <danish_cyborg@hotmail.com>
2017-01-04 18:31:51 +0100

    Fixed invalid target error message not being displayed when inviting nothing to a group

--- scripts/commands/generic/groupInvite.js
+++ scripts/commands/generic/groupInvite.js
@@ -1,14 +1,15 @@
 function executeCommand(galacticManager, player, target, args) {
     var GroupEventIntent = Java.type("intents.GroupEventIntent");
     var GroupEventType = Java.type("intents.GroupEventIntent.GroupEventType");
-
-	if (target) {
+	var farAwayTarget;
+	
+	if (args) {
+		farAwayTarget = galacticManager.getPlayerManager().getPlayerByCreatureFirstName(args);
+	}
+	
+	if (farAwayTarget) {
+		new GroupEventIntent(GroupEventType.GROUP_INVITE, player, farAwayTarget.getCreatureObject()).broadcast();
+	} else {
 		new GroupEventIntent(GroupEventType.GROUP_INVITE, player, target).broadcast();
-	} else if (args) {
-		var farAwayTarget = galacticManager.getPlayerManager().getPlayerByCreatureFirstName(args);
-		
-		if (farAwayTarget) {
-			new GroupEventIntent(GroupEventType.GROUP_INVITE, player, farAwayTarget.getCreatureObject()).broadcast();
-		}
 	}
 }
