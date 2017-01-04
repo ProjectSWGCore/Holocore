b27e3f46cf316086831d9959c82d017503b50d12
Mads Boddum <danish_cyborg@hotmail.com>
2017-01-04 18:07:10 +0100

    Added support for inviting players to a group that are outside your awareness

--- scripts/commands/generic/groupInvite.js
+++ scripts/commands/generic/groupInvite.js
@@ -2,5 +2,13 @@ function executeCommand(galacticManager, player, target, args) {
     var GroupEventIntent = Java.type("intents.GroupEventIntent");
     var GroupEventType = Java.type("intents.GroupEventIntent.GroupEventType");
 
-    new GroupEventIntent(GroupEventType.GROUP_INVITE, player, target).broadcast();
+	if (target) {
+		new GroupEventIntent(GroupEventType.GROUP_INVITE, player, target).broadcast();
+	} else if (args) {
+		var farAwayTarget = galacticManager.getPlayerManager().getPlayerByCreatureFirstName(args);
+		
+		if (farAwayTarget) {
+			new GroupEventIntent(GroupEventType.GROUP_INVITE, player, farAwayTarget.getCreatureObject()).broadcast();
+		}
+	}
 }
