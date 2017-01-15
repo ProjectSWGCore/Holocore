bda15cfedca06676f5f30d1d5bb6dc80ff78f3a4
tacef ine <tacef@projectswg.com>
2017-01-15 07:19:18 +0100

    Updated ObjControllers with License

--- src/network/packets/swg/zone/object_controller/CraftingSessionEnded.java
+++ src/network/packets/swg/zone/object_controller/CraftingSessionEnded.java
@@ -1,3 +1,30 @@
+/************************************************************************************
+ * Copyright (c) 2015 /// Project SWG /// www.projectswg.com                        *
+ *                                                                                  *
+ * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on           *
+ * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies.  *
+ * Our goal is to create an emulator which will provide a server for players to     *
+ * continue playing a game similar to the one they used to play. We are basing      *
+ * it on the final publish of the game prior to end-game events.                    *
+ *                                                                                  *
+ * This file is part of Holocore.                                                   *
+ *                                                                                  *
+ * -------------------------------------------------------------------------------- *
+ *                                                                                  *
+ * Holocore is free software: you can redistribute it and/or modify                 *
+ * it under the terms of the GNU Affero General Public License as                   *
+ * published by the Free Software Foundation, either version 3 of the               *
+ * License, or (at your option) any later version.                                  *
+ *                                                                                  *
+ * Holocore is distributed in the hope that it will be useful,                      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ * GNU Affero General Public License for more details.                              *
+ *                                                                                  *
+ * You should have received a copy of the GNU Affero General Public License         *
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.                *
+ *                                                                                  *
+ ***********************************************************************************/
 package network.packets.swg.zone.object_controller;
 
 import java.nio.ByteBuffer;
@@ -27,7 +54,7 @@ public class CraftingSessionEnded extends ObjectController{
 		decodeHeader(data);
 		playerId = getLong(data);
 		sessionId = getInt(data);
-		count = getByte(data);		
+		count = getByte(data);
 	}
 
 	@Override
@@ -62,5 +89,5 @@ public class CraftingSessionEnded extends ObjectController{
 
 	public void setCount(byte count) {
 		this.count = count;
-	}	
+	}
 }
