8ba2dde953481fb55458e05cebb0dc015c0c1757
Eric Frechette <frechetta93@gmail.com>
2017-03-30 22:07:03 -0700

    Adjusted login.sql script: made 'username' the primary key for 'users' table (there should be no two accounts with the same username); added line to create default user.

--- serverdata/login/login.sql
+++ serverdata/login/login.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS users (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	username TEXT NOT NULL,
+	username TEXT PRIMARY KEY,
 	password TEXT NOT NULL,
 	email TEXT NOT NULL,
 	access_level TEXT NOT NULL,
 	banned INTEGER NOT NULL DEFAULT FALSE
 );
 
+INSERT OR IGNORE INTO users(username, password, email, access_level) VALUES('holocore', 'password', 'holocore@projectswg.com', 'csr');
+
 CREATE TABLE IF NOT EXISTS players (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL,
 	race TEXT NOT NULL,
-	userid INTEGER NOT NULL
+	username TEXT NOT NULL
 );
