a7c5b3af0170edca37737f406f2c8b1a8b95e883
Mads Boddum <danish_cyborg@hotmail.com>
2017-07-09 11:45:17 +0200

    Pets are now stored when teleporting

--- src/services/objects/PetService.java
+++ src/services/objects/PetService.java
@@ -1,12 +1,41 @@
+/***********************************************************************************
+* Copyright (c) 2015 /// Project SWG /// www.projectswg.com                        *
+*                                                                                  *
+* ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on           *
+* July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies.  *
+* Our goal is to create an emulator which will provide a server for players to     *
+* continue playing a game similar to the one they used to play. We are basing      *
+* it on the final publish of the game prior to end-game events.                    *
+*                                                                                  *
+* This file is part of Holocore.                                                   *
+*                                                                                  *
+* -------------------------------------------------------------------------------- *
+*                                                                                  *
+* Holocore is free software: you can redistribute it and/or modify                 *
+* it under the terms of the GNU Affero General Public License as                   *
+* published by the Free Software Foundation, either version 3 of the               *
+* License, or (at your option) any later version.                                  *
+*                                                                                  *
+* Holocore is distributed in the hope that it will be useful,                      *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+* GNU Affero General Public License for more details.                              *
+*                                                                                  *
+* You should have received a copy of the GNU Affero General Public License         *
+* along with Holocore.  If not, see <http://www.gnu.org/licenses/>.                *
+*                                                                                  *
+***********************************************************************************/
 package services.objects;
 
 import com.projectswg.common.control.Service;
 import com.projectswg.common.data.location.Location;
 import com.projectswg.common.debug.Log;
+import intents.ObjectTeleportedIntent;
 import intents.PlayerEventIntent;
 import intents.object.ContainerTransferIntent;
 import intents.object.DestroyObjectIntent;
 import intents.object.ObjectCreatedIntent;
+import intents.object.ObjectTeleportIntent;
 import intents.radial.RadialRequestIntent;
 import intents.radial.RadialResponseIntent;
 import intents.radial.RadialSelectionIntent;
@@ -20,30 +49,50 @@ import resources.radial.RadialItem;
 import resources.radial.RadialOption;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Mads on 19-06-2017.
  */
 public final class PetService extends Service {
 
-	// TODO no-vehicle zones
+	// TODO no-vehicle zones (does not affect creature mounts)
+
+	private final Map<CreatureObject, Collection<Pet>> calledPets;
+
 
 	public PetService() {
+		calledPets = new HashMap<>();
+
+		registerForIntent(ObjectTeleportIntent.class, oti -> handleObjectTeleport(oti));
 		registerForIntent(PlayerEventIntent.class, pei -> handlePlayerEvent(pei));
 		registerForIntent(RadialRequestIntent.class, rri -> handleRadialRequest(rri));
 		registerForIntent(RadialSelectionIntent.class, rsi -> handleRadialSelection(rsi));
 	}
 
+	private void handleObjectTeleport(ObjectTeleportIntent oti) {
+		SWGObject object = oti.getObject();
+
+		if (!(object instanceof CreatureObject)) {
+			return;
+		}
+
+		storePets((CreatureObject) object);
+	}
+
 	private void handlePlayerEvent(PlayerEventIntent pei) {
 		switch (pei.getEvent()) {
-		//	case PE_LOGGED_OUT: storePets(); break;	// TODO store vehicle when teleporting or logging out
+			case PE_LOGGED_OUT: storePets(pei.getPlayer().getCreatureObject()); break;	// Store pets when logging out
 		}
 	}
 
 	@Override
 	public boolean terminate() {
-		// TODO store every pet called out by every player
+		storePets();
+
 		return super.terminate();
 	}
 
@@ -98,7 +147,7 @@ public final class PetService extends Service {
 		String mobileTemplate = mobileForVehicleDeed(deedTemplate);
 		String pcdTemplate = pcdForVehicleDeed(deedTemplate);
 		CreatureObject creator = player.getCreatureObject();
-		CreatureObject vehicle = ObjectCreator.createObjectFromTemplate(mobileTemplate, CreatureObject.class);	// TODO don't force CreatureObject type
+		CreatureObject vehicle = (CreatureObject) ObjectCreator.createObjectFromTemplate(mobileTemplate);
 
 		if (vehicle == null) {
 			Log.e("Vehicle not generated: Invalid mobile template %s", mobileTemplate);
@@ -136,8 +185,7 @@ public final class PetService extends Service {
 		CreatureObject vehicle = (CreatureObject) object;
 		CreatureObject requester = player.getCreatureObject();
 
-//		requester.moveToContainer(vehicle);	// Put requester in the vehicle object
-		new ContainerTransferIntent(requester, vehicle).broadcast();
+		requester.moveToContainer(vehicle);	// Put requester in the vehicle object
 		requester.setStatesBitmask(CreatureState.RIDING_MOUNT);
 
 		vehicle.setStatesBitmask(CreatureState.MOUNTED_CREATURE);
@@ -173,7 +221,19 @@ public final class PetService extends Service {
 		return deedTemplate.replace("tangible/deed/vehicle_deed", "intangible/vehicle").replace("deed", "pcd");
 	}
 
-	private void callPet(CreatureObject pet, Location location) {
+	private void callPet(CreatureObject caller, CreatureObject pet, Location location) {
+		Collection<Pet> callerPets;
+
+		if (calledPets.containsKey(caller)) {
+			callerPets = calledPets.get(caller);
+
+			// TODO check if size of callerPets exceeds max allowed
+		} else {
+			callerPets = new ArrayList<>();
+			calledPets.put(caller, callerPets);
+		}
+
+		callerPets.add(new Pet(pet.getParent(), pet));
 		// TODO no calling during combat
 		// TODO after combat there's a delay
 		pet.setLocation(location);
@@ -182,19 +242,61 @@ public final class PetService extends Service {
 		pet.moveToContainer(null);	// Move pet from Pet Control Device into the world
 	}
 
-	private void storePet(CreatureObject pet) {
-		// TODO transfer into correct PCD
+	/**
+	* Stores the specified pet
+	*/
+	private void storePet(CreatureObject pet, SWGObject petControlDevice) {
+		pet.moveToContainer(petControlDevice);
 	}
 
 	/**
-	 * Stores any pets that might be called
+	 * Stores any pets that might be called by the specified player
 	 */
-	private void storePets(CreatureObject player) {
-		// TODO check for PCDs in datapad, store the pet for each one of them
+	private void storePets(CreatureObject player) {	
+		if (!calledPets.containsKey(player)) {
+			return;
+		}
+
+		Collection<Pet> pets = calledPets.remove(player);
+
+		for (Pet pet : pets) {
+			pet.getCreature().moveToContainer(pet.getPetControlDevice());
+		}
+	}
+
+	/**
+	* Stores all called pets by all players
+	*/
+	private void storePets() {
+		Collection<CreatureObject> petMasters = calledPets.keySet();
+
+		for (CreatureObject petMaster : petMasters) {
+			storePets(petMaster);
+		}
 	}
 
 	private boolean isMountable(SWGObject object) {
 		return object instanceof CreatureObject && ((CreatureObject) object).hasOptionFlags(OptionFlag.MOUNT);
 	}
+
+	private static class Pet {
+		private final SWGObject petControlDevice;
+		private final CreatureObject creature;
+
+		private Pet(SWGObject petControlDevice, CreatureObject creature) {
+			this.creature = creature;
+			this.pet = pet;
+		}
+
+		private SWGObject getPetControlDevice() {
+			return petControlDevice;
+		}
+
+		private CreatureObject getCreature() {
+			return creature;
+		}
+
+	}
+
 
 }
