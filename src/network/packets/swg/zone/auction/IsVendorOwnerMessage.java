c6afb86d3db8e3c785c59e33a213df0cbc1f6572
Tacef Ine <tacef@projectswg.com>
2017-08-11 18:03:00 +0200

    Renamed IsVendorMessage to be accurate

---
+++ src/network/packets/swg/zone/auction/IsVendorOwnerMessage.java
@@ -0,0 +1,32 @@
+package network.packets.swg.zone.auction;
+
+import com.projectswg.common.network.NetBuffer;
+
+import network.packets.swg.SWGPacket;
+
+public class IsVendorOwnerMessage extends SWGPacket {
+
+	public static final int CRC = com.projectswg.common.data.CRC.getCrc("IsVendorOwnerMessage");
+	
+	private long terminalId;	
+	
+	public long getTerminalId() {
+		return terminalId;
+	}
+
+	@Override
+	public void decode(NetBuffer data) {
+		if (!super.checkDecode(data, CRC))
+			return;		
+		terminalId = data.getLong();
+	}
+
+	@Override
+	public NetBuffer encode() {
+		NetBuffer data = NetBuffer.allocate(14);
+		data.addShort(2);
+		data.addInt(CRC);
+		data.addLong(terminalId);
+		return data;
+	}
+}
