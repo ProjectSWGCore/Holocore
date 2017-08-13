f1d28e9117d1231017e8d9e746c228eadadc764a
Tacef Ine <tacef@projectswg.com>
2017-08-13 18:37:48 +0200

    Moved Command from script to Callback Removed Requestdraftslotsbatch command since unused

---
+++ src/resources/commands/callbacks/RequestDraftSlotsCallback.java
@@ -0,0 +1,17 @@
+package resources.commands.callbacks;
+
+import network.packets.swg.zone.object_controller.DraftSlotsQueryResponse;
+import resources.commands.ICmdCallback;
+import resources.objects.SWGObject;
+import resources.player.Player;
+import resources.schematic.DraftSchematic;
+import services.galaxy.GalacticManager;
+
+public class RequestDraftSlotsCallback implements ICmdCallback{
+
+	@Override
+	public void execute(GalacticManager galacticManager, Player player, SWGObject target, String args) {
+		DraftSchematic schematic = new DraftSchematic();		
+		player.getPlayerObject().sendSelf(new DraftSlotsQueryResponse(schematic));
+	}
+}
