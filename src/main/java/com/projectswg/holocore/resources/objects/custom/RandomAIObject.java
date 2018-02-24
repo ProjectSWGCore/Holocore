2c43f19e6475c63e641d7d314ad194a216b66471
Emilie Jørgensen <emiliemarott@outlook.dk>
2018-02-24 21:56:20 +0100

    Fixed main location for loitering AI

--- src/main/java/com/projectswg/holocore/resources/objects/custom/RandomAIObject.java
+++ src/main/java/com/projectswg/holocore/resources/objects/custom/RandomAIObject.java
@@ -44,7 +44,11 @@ public class RandomAIObject extends AIObject {
 	public Location getMainLocation() {
 		return mainLocation;
 	}
-	
+
+	public void setMainLocation(Location mainLocation) {
+		this.mainLocation = mainLocation;
+	}
+
 	@Override
 	protected void aiInitialize() {
 		super.aiInitialize();
@@ -55,7 +59,10 @@ public class RandomAIObject extends AIObject {
 	@Override
 	public void aiStart() {
 		super.aiStart();
-		this.mainLocation = getLocation();
+		if(mainLocation == null){
+			// If no location is given, then use object location
+			setMainLocation(getLocation());
+		}
 	}
 	
 	@Override
