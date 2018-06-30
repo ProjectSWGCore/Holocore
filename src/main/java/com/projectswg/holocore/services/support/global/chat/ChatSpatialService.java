3f77af339ec382306dbe7fc4fa8dc103469103b5
Obique <obique.pswg@gmail.com>
2018-06-29 21:20:13 -0500

    Added chat range config option, with default set to 128m

--- src/main/java/com/projectswg/holocore/services/support/global/chat/ChatSpatialService.java
+++ src/main/java/com/projectswg/holocore/services/support/global/chat/ChatSpatialService.java
@@ -2,15 +2,20 @@ package com.projectswg.holocore.services.support.global.chat;
 
 import com.projectswg.common.network.packets.swg.zone.object_controller.SpatialChat;
 import com.projectswg.holocore.intents.support.global.chat.SpatialChatIntent;
+import com.projectswg.holocore.resources.support.data.config.ConfigFile;
+import com.projectswg.holocore.resources.support.data.server_info.DataManager;
 import com.projectswg.holocore.resources.support.objects.swg.SWGObject;
 import com.projectswg.holocore.resources.support.global.player.Player;
+import com.projectswg.holocore.resources.support.objects.swg.creature.CreatureObject;
 import me.joshlarson.jlcommon.control.IntentHandler;
 import me.joshlarson.jlcommon.control.Service;
 
 public class ChatSpatialService extends Service {
 	
+	private final int chatRange;
+	
 	public ChatSpatialService() {
-		
+		this.chatRange = DataManager.getConfig(ConfigFile.PRIMARY).getInt("CHAT-SPATIAL-RANGE", 128);
 	}
 	
 	@IntentHandler
@@ -26,7 +31,10 @@ public class ChatSpatialService extends Service {
 		for (Player owner : actor.getObservers()) {
 			if (owner.getPlayerObject().isIgnored(senderName))
 				continue;
-			owner.sendPacket(new SpatialChat(owner.getCreatureObject().getObjectId(), message));
+			CreatureObject creature = owner.getCreatureObject();
+			if (creature.getWorldLocation().distanceTo(actor.getWorldLocation()) > chatRange)
+				continue;
+			owner.sendPacket(new SpatialChat(creature.getObjectId(), message));
 		}
 	}
 	
