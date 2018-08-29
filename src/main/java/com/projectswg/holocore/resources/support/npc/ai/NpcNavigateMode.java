2acac430c5e21f92202213dd29e053f6c716a1c1
Obique <obique.pswg@gmail.com>
2018-08-29 18:05:40 -0500

    Fixed NPCs disappearing when driving past them fast enough

--- src/main/java/com/projectswg/holocore/resources/support/npc/ai/NpcNavigateMode.java
+++ src/main/java/com/projectswg/holocore/resources/support/npc/ai/NpcNavigateMode.java
@@ -6,23 +6,25 @@ import com.projectswg.holocore.resources.support.objects.swg.custom.AIObject;
 import com.projectswg.holocore.resources.support.objects.swg.custom.NpcMode;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class NpcNavigateMode extends NpcMode {
 	
 	private final NavigationPoint destination;
+	private final AtomicBoolean startedMovement;
 	
 	public NpcNavigateMode(@NotNull AIObject obj, @NotNull NavigationPoint destination) {
 		super(obj);
 		this.destination = destination;
-	}
-	
-	@Override
-	public void onModeStart() {
-		StartNpcMovementIntent.broadcast(getAI(), destination.getParent(), destination.getLocation(), destination.getSpeed());
+		this.startedMovement = new AtomicBoolean(false);
 	}
 	
 	@Override
 	public void act() {
 		if (destination.distanceTo(getAI()) > 1E-3) {
+			if (!startedMovement.getAndSet(true)) {
+				StartNpcMovementIntent.broadcast(getAI(), destination.getParent(), destination.getLocation(), destination.getSpeed());
+			}
 			queueNextLoop(1000);
 		} else {
 			ScheduleNpcModeIntent.broadcast(getAI(), null);
