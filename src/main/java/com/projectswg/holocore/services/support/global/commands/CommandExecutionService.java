86a251573caadd81163ecfb8ec14e5e52d163893
Mads Boddum <madsboddum@outlook.dk>
2019-03-02 14:56:20 +0100

    #101: /admin -> /invulnerable

--- src/main/java/com/projectswg/holocore/services/support/global/commands/CommandExecutionService.java
+++ src/main/java/com/projectswg/holocore/services/support/global/commands/CommandExecutionService.java
@@ -109,7 +109,7 @@ public class CommandExecutionService extends Service {
 		registerCallback("credits", CmdMoney::new);
 		registerCallback("setSpeed", CmdSetSpeed::new);
 		registerCallback("setExperience", CmdSetExperience::new);
-		registerCallback("admin", CmdInvulnerable::new);
+		registerCallback("invulnerable", CmdInvulnerable::new);
 	}
 	
 	private void addChatScripts() {
