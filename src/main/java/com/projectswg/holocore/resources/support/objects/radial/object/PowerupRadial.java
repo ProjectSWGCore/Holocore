266ed304838f762c8c73b9497a4fd00ea0d3218f
Mads Boddum <madsboddum@outlook.dk>
2019-03-03 03:24:38 +0100

    #33: Removed unused import from PowerupRadial

--- src/main/java/com/projectswg/holocore/resources/support/objects/radial/object/PowerupRadial.java
+++ src/main/java/com/projectswg/holocore/resources/support/objects/radial/object/PowerupRadial.java
@@ -3,7 +3,6 @@ package com.projectswg.holocore.resources.support.objects.radial.object;
 import com.projectswg.common.data.radial.RadialItem;
 import com.projectswg.common.data.radial.RadialOption;
 import com.projectswg.holocore.intents.gameplay.combat.buffs.PowerupIntent;
-import com.projectswg.holocore.intents.gameplay.combat.loot.OpenRareChestIntent;
 import com.projectswg.holocore.resources.support.global.player.Player;
 import com.projectswg.holocore.resources.support.objects.swg.SWGObject;
 import com.projectswg.holocore.resources.support.objects.swg.tangible.TangibleObject;
