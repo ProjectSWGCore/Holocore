933009cdf71fb783c3fe6bba66b22929891e1e54
Obique <obique.pswg@gmail.com>
2019-10-14 10:46:34 -0400

    Converted ChatRoomLogWrapper to Kotlin

--- src/main/java/com/projectswg/holocore/utilities/ChatRoomLogWrapper.kt
+++ src/main/java/com/projectswg/holocore/utilities/ChatRoomLogWrapper.kt
@@ -1,46 +1,38 @@
 /***********************************************************************************
  * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
- *                                                                                 *
+ * *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
  * Our goal is to create an emulator which will provide a server for players to    *
  * continue playing a game similar to the one they used to play. We are basing     *
  * it on the final publish of the game prior to end-game events.                   *
- *                                                                                 *
+ * *
  * This file is part of Holocore.                                                  *
- *                                                                                 *
+ * *
  * --------------------------------------------------------------------------------*
- *                                                                                 *
+ * *
  * Holocore is free software: you can redistribute it and/or modify                *
  * it under the terms of the GNU Affero General Public License as                  *
  * published by the Free Software Foundation, either version 3 of the              *
  * License, or (at your option) any later version.                                 *
- *                                                                                 *
+ * *
  * Holocore is distributed in the hope that it will be useful,                     *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
  * GNU Affero General Public License for more details.                             *
- *                                                                                 *
+ * *
  * You should have received a copy of the GNU Affero General Public License        *
- * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
- ***********************************************************************************/
-package com.projectswg.holocore.utilities;
+ * along with Holocore.  If not, see <http:></http:>//www.gnu.org/licenses/>.               *
+ */
+package com.projectswg.holocore.utilities
 
-import com.projectswg.holocore.intents.support.global.chat.SystemChatRoomMessageIntent;
-import me.joshlarson.jlcommon.log.Log;
-import me.joshlarson.jlcommon.log.LogWrapper;
-import org.jetbrains.annotations.NotNull;
+import com.projectswg.holocore.intents.support.global.chat.SystemChatRoomMessageIntent
+import me.joshlarson.jlcommon.log.Log
+import me.joshlarson.jlcommon.log.LogWrapper
 
-public class ChatRoomLogWrapper implements LogWrapper {
-
-	private final String roomPath;
-
-	public ChatRoomLogWrapper(String roomPath) {
-		this.roomPath = roomPath;
-	}
-
-	@Override
-	public void onLog(Log.@NotNull LogLevel level, @NotNull String str) {
-		SystemChatRoomMessageIntent.broadcast(roomPath, str);
+class ChatRoomLogWrapper(private val roomPath: String) : LogWrapper {
+	
+	override fun onLog(level: Log.LogLevel, str: String) {
+		SystemChatRoomMessageIntent.broadcast(roomPath, str)
 	}
 }
