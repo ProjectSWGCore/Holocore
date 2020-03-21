0a95627852c4c5528a4c9bfcf2a0457ee857320d
Obique <obique.pswg@gmail.com>
2020-03-21 11:49:54 -0400

    Removed unit test for passing null value to NotNull annotated type

--- src/test/java/com/projectswg/holocore/resources/support/data/server_info/loader/TestTerrainLevelLoader.java
+++ src/test/java/com/projectswg/holocore/resources/support/data/server_info/loader/TestTerrainLevelLoader.java
@@ -1,23 +1,15 @@
 package com.projectswg.holocore.resources.support.data.server_info.loader;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
 public class TestTerrainLevelLoader {
 	
-	private TerrainLevelLoader loader;
-	
-	@Before
+	@Test
 	public void setup() throws IOException {
-		loader = new TerrainLevelLoader();
+		var loader = new TerrainLevelLoader();
 		loader.load();	// We just expect this to not throw an exception
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testGetTerrainLevelInfoNull() {
-		loader.getTerrainLevelInfo(null);
 	}
 	
 }
