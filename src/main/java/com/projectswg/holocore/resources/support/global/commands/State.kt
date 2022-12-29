6e1dcc6a0584bac5e17cd44e595bbe2678cfeed6
Ziggy <ziggyswg@outlook.com>
2022-12-29 13:50:40 +0100

    Added support for stunning, which will pause action regeneration while active
    This is a big enabler in making a lot of commands work, as many
    professions have stun abilities.

--- src/main/java/com/projectswg/holocore/resources/support/global/commands/State.kt
+++ src/main/java/com/projectswg/holocore/resources/support/global/commands/State.kt
@@ -5,6 +5,7 @@ import com.projectswg.holocore.resources.support.objects.swg.creature.CreatureSt
 
 enum class State(val stateTableId: Int, val commandSdbColumnName: String, private val stateCheck: StateCheck) {
 	COMBAT(1, "S:combat", CreatureStateCheck(CreatureState.COMBAT)),
+	STUNNED(12, "S:stunned", CreatureStateCheck(CreatureState.STUNNED)),
 	BLEEDING(24, "S:bleeding", CreatureStateCheck(CreatureState.BLEEDING));
 
 	fun isActive(creatureObject: CreatureObject): Boolean {
