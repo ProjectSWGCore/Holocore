9fe87f03c0a87eee43a5e7c90744ffb7d9344f58
Ziggy <ziggyswg@outlook.com>
2023-04-01 21:59:15 +0200

    Fixed issue where some looted items had "Volume: 0" when examining them
    This won't fix existing items.

--- src/main/java/com/projectswg/holocore/services/gameplay/combat/loot/generation/ItemLootGenerator.kt
+++ src/main/java/com/projectswg/holocore/services/gameplay/combat/loot/generation/ItemLootGenerator.kt
@@ -1,5 +1,5 @@
 /***********************************************************************************
- * Copyright (c) 2019 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
@@ -74,7 +74,9 @@ class ItemLootGenerator {
 						SystemMessageIntent(killer.owner!!, "We don't support this loot item yet: $itemName").broadcast()
 					}
 					itemName.endsWith(".iff") -> {
-						loot.add(ObjectCreator.createObjectFromTemplate(itemName))
+						val element = ObjectCreator.createObjectFromTemplate(itemName)
+						element.volume = 1
+						loot.add(element)
 					}
 					else -> {
 						val obj = StaticItemCreator.createItem(itemName)
