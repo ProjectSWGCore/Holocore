0c38db121fa149751014d1bde817628516287936
Ziggy <ziggyswg@outlook.com>
2023-04-02 23:33:49 +0200

    [General] Fixed a bug where you had to relog to see new names for waypoints
    Relates to GitHub issue #749

--- src/main/java/com/projectswg/holocore/resources/support/objects/swg/player/PlayerObjectOwner.java
+++ src/main/java/com/projectswg/holocore/resources/support/objects/swg/player/PlayerObjectOwner.java
@@ -1,5 +1,5 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
@@ -102,9 +102,9 @@ class PlayerObjectOwner implements MongoPersistable {
 		}
 	}
 	
-	public void updateWaypoint(WaypointObject obj) {
+	public void updateWaypoint(WaypointObject waypoint) {
 		synchronized (waypoints) {
-			waypoints.update(obj.getObjectId());
+			waypoints.update(waypoint.getObjectId());
 			waypoints.sendDeltaMessage(obj);
 		}
 	}
