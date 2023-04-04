d450c43177e2d81c3ebe7133a4cb8153db317128
Ziggy <ziggyswg@outlook.com>
2023-04-04 11:54:29 +0200

    [Entertainer] Instruments and Props have their required skills set
    Relates to GitHub issue #697

--- src/main/java/com/projectswg/holocore/resources/support/data/server_info/loader/StaticItemLoader.kt
+++ src/main/java/com/projectswg/holocore/resources/support/data/server_info/loader/StaticItemLoader.kt
@@ -1,29 +1,29 @@
 /***********************************************************************************
- * Copyright (c) 2019 /// Project SWG /// www.projectswg.com                       *
- * *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
+ *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
  * Our goal is to create an emulator which will provide a server for players to    *
  * continue playing a game similar to the one they used to play. We are basing     *
  * it on the final publish of the game prior to end-game events.                   *
- * *
+ *                                                                                 *
  * This file is part of Holocore.                                                  *
- * *
+ *                                                                                 *
  * --------------------------------------------------------------------------------*
- * *
+ *                                                                                 *
  * Holocore is free software: you can redistribute it and/or modify                *
  * it under the terms of the GNU Affero General Public License as                  *
  * published by the Free Software Foundation, either version 3 of the              *
  * License, or (at your option) any later version.                                 *
- * *
+ *                                                                                 *
  * Holocore is distributed in the hope that it will be useful,                     *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
  * GNU Affero General Public License for more details.                             *
- * *
+ *                                                                                 *
  * You should have received a copy of the GNU Affero General Public License        *
- * along with Holocore.  If not, see <http:></http:>//www.gnu.org/licenses/>.               *
- */
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
+ ***********************************************************************************/
 
 package com.projectswg.holocore.resources.support.data.server_info.loader
 
@@ -186,7 +186,7 @@ class StaticItemLoader internal constructor() : DataLoader() {
 			get() = field.clone()
 		val value: Int				= set.getInt("value").toInt()
 		val isUnique: Boolean		= set.getInt("isUnique") != 0L
-		
+		val requiredSkill: String	= set.getText("required_skill")
 	}
 	
 	class ObjectItemInfo(set: SdbResultSet, colorArray: SdbIntegerColumnArraySet) {
@@ -238,21 +238,21 @@ class StaticItemLoader internal constructor() : DataLoader() {
 		val woundChance: Int = set.getInt("wound_chance").toInt()
 		
 		init {
-			when (set.getText("weapon_type")) {
-				"RIFLE" -> weaponType = WeaponType.RIFLE
-				"CARBINE" -> weaponType = WeaponType.CARBINE
-				"PISTOL" -> weaponType = WeaponType.PISTOL
-				"HEAVY" -> weaponType = WeaponType.HEAVY
-				"ONE_HANDED_MELEE" -> weaponType = WeaponType.ONE_HANDED_MELEE
-				"TWO_HANDED_MELEE" -> weaponType = WeaponType.TWO_HANDED_MELEE
-				"UNARMED" -> weaponType = WeaponType.UNARMED
-				"POLEARM_MELEE" -> weaponType = WeaponType.POLEARM_MELEE
-				"THROWN" -> weaponType = WeaponType.THROWN
-				"ONE_HANDED_SABER" -> weaponType = WeaponType.ONE_HANDED_SABER
-				"TWO_HANDED_SABER" -> weaponType = WeaponType.TWO_HANDED_SABER
-				"POLEARM_SABER" -> weaponType = WeaponType.POLEARM_SABER
+			weaponType = when (set.getText("weapon_type")) {
+				"RIFLE" -> WeaponType.RIFLE
+				"CARBINE" -> WeaponType.CARBINE
+				"PISTOL" -> WeaponType.PISTOL
+				"HEAVY" -> WeaponType.HEAVY
+				"ONE_HANDED_MELEE" -> WeaponType.ONE_HANDED_MELEE
+				"TWO_HANDED_MELEE" -> WeaponType.TWO_HANDED_MELEE
+				"UNARMED" -> WeaponType.UNARMED
+				"POLEARM_MELEE" -> WeaponType.POLEARM_MELEE
+				"THROWN" -> WeaponType.THROWN
+				"ONE_HANDED_SABER" -> WeaponType.ONE_HANDED_SABER
+				"TWO_HANDED_SABER" -> WeaponType.TWO_HANDED_SABER
+				"POLEARM_SABER" -> WeaponType.POLEARM_SABER
 				else -> throw IllegalArgumentException("weapon_type is unrecognized: " + set.getText("weapon_type"))
-			}// pre-NGE artifact for pre-NGE heavy weapons
+			}
 		}
 		
 		private fun getDamageType(str: String): DamageType? {
