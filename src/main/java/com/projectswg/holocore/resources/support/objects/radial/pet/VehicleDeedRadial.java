a059900dd0e5aa417e8da832b3d2bed7677ea61a
Ziggy <ziggyswg@outlook.com>
2023-04-04 13:10:29 +0200

    [Vehicles] The radial option for vehicle deeds should be "Generate", not "Call"
    Relates to GitHub issue #1219

--- src/main/java/com/projectswg/holocore/resources/support/objects/radial/pet/VehicleDeedRadial.java
+++ src/main/java/com/projectswg/holocore/resources/support/objects/radial/pet/VehicleDeedRadial.java
@@ -1,5 +1,5 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
@@ -7,22 +7,22 @@
  * continue playing a game similar to the one they used to play. We are basing     *
  * it on the final publish of the game prior to end-game events.                   *
  *                                                                                 *
- * This file is part of PSWGCommon.                                                *
+ * This file is part of Holocore.                                                  *
  *                                                                                 *
  * --------------------------------------------------------------------------------*
  *                                                                                 *
- * PSWGCommon is free software: you can redistribute it and/or modify              *
+ * Holocore is free software: you can redistribute it and/or modify                *
  * it under the terms of the GNU Affero General Public License as                  *
  * published by the Free Software Foundation, either version 3 of the              *
  * License, or (at your option) any later version.                                 *
  *                                                                                 *
- * PSWGCommon is distributed in the hope that it will be useful,                   *
+ * Holocore is distributed in the hope that it will be useful,                     *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
  * GNU Affero General Public License for more details.                             *
  *                                                                                 *
  * You should have received a copy of the GNU Affero General Public License        *
- * along with PSWGCommon.  If not, see <http://www.gnu.org/licenses/>.             *
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
  ***********************************************************************************/
 
 package com.projectswg.holocore.resources.support.objects.radial.pet;
@@ -45,7 +45,7 @@ public class VehicleDeedRadial implements RadialHandlerInterface {
 	
 	@Override
 	public void getOptions(Collection<RadialOption> options, Player player, SWGObject target) {
-		options.add(RadialOption.create(RadialItem.VEHICLE_GENERATE));
+		options.add(RadialOption.create(RadialItem.VEHICLE_GENERATE, "@pet/pet_menu:menu_generate"));
 		options.add(RadialOption.create(RadialItem.ITEM_DESTROY));
 		options.add(RadialOption.create(RadialItem.EXAMINE));
 	}
