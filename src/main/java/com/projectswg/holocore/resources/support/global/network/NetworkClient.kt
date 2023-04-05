818b5ecca0ded76e697455437bcc164b467b3dc8
Ziggy <ziggyswg@outlook.com>
2023-04-05 19:46:46 +0200

    The log statement has a bug with the "Expected" part, where it displays the amount of bytes in excess instead of the expected buffer position

--- src/main/java/com/projectswg/holocore/resources/support/global/network/NetworkClient.kt
+++ src/main/java/com/projectswg/holocore/resources/support/global/network/NetworkClient.kt
@@ -1,29 +1,29 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
- * *
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
+ *                                                                                 *
  * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
  * Our goal is to create an emulator which will provide a server for players to    *
  * continue playing a game similar to the one they used to play. We are basing     *
  * it on the final publish of the game prior to end-game events.                   *
- * *
+ *                                                                                 *
  * This file is part of Holocore.                                                  *
- * *
+ *                                                                                 *
  * --------------------------------------------------------------------------------*
- * *
+ *                                                                                 *
  * Holocore is free software: you can redistribute it and/or modify                *
  * it under the terms of the GNU Affero General Public License as                  *
  * published by the Free Software Foundation, either version 3 of the              *
  * License, or (at your option) any later version.                                 *
- * *
+ *                                                                                 *
  * Holocore is distributed in the hope that it will be useful,                     *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
  * GNU Affero General Public License for more details.                             *
- * *
+ *                                                                                 *
  * You should have received a copy of the GNU Affero General Public License        *
- * along with Holocore.  If not, see <http:></http:>//www.gnu.org/licenses/>.               *
- */
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
+ ***********************************************************************************/
 package com.projectswg.holocore.resources.support.global.network
 
 import com.projectswg.common.network.NetBuffer
@@ -35,7 +35,6 @@ import com.projectswg.common.network.packets.swg.admin.AdminPacket
 import com.projectswg.common.network.packets.swg.holo.HoloConnectionStarted
 import com.projectswg.common.network.packets.swg.holo.HoloConnectionStopped
 import com.projectswg.common.network.packets.swg.holo.HoloConnectionStopped.ConnectionStoppedReason
-import com.projectswg.common.network.packets.swg.zone.chat.ChatSystemMessage
 import com.projectswg.common.network.packets.swg.zone.object_controller.ObjectController
 import com.projectswg.holocore.ProjectSWG
 import com.projectswg.holocore.intents.support.global.login.RequestLoginIntent
@@ -215,7 +214,7 @@ class NetworkClient(private val remoteAddress: SocketAddress, write: (ByteBuffer
 		if (allowOutbound(p) && connected.get()) {
 			val encoded = p.encode()
 			if (encoded.position() != encoded.capacity())
-				Log.w("SWGPacket %s has invalid array length. Expected: %d  Actual: %d", p, encoded.remaining(), encoded.capacity())
+				Log.w("SWGPacket %s has invalid array length. Expected: %d  Actual: %d", p, encoded.position(), encoded.capacity())
 			
 			writeLock.withLock {
 				wsProtocol.send(WebsocketFrame(WebsocketFrameType.BINARY, encoded.buffer.array()))
