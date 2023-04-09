68120780d25e47c8457a86a8443b7adff82cf31b
Josh Larson <joshlarson2015@gmail.com>
2023-04-09 16:55:17 -0500

    Added missing delegate

---
+++ src/main/java/com/projectswg/holocore/resources/support/objects/swg/IndirectBaselineDelegate.kt
@@ -0,0 +1,48 @@
+/***********************************************************************************
+ * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
+ *                                                                                 *
+ * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
+ * Our goal is to create an emulator which will provide a server for players to    *
+ * continue playing a game similar to the one they used to play. We are basing     *
+ * it on the final publish of the game prior to end-game events.                   *
+ *                                                                                 *
+ * This file is part of Holocore.                                                  *
+ *                                                                                 *
+ * --------------------------------------------------------------------------------*
+ *                                                                                 *
+ * Holocore is free software: you can redistribute it and/or modify                *
+ * it under the terms of the GNU Affero General Public License as                  *
+ * published by the Free Software Foundation, either version 3 of the              *
+ * License, or (at your option) any later version.                                 *
+ *                                                                                 *
+ * Holocore is distributed in the hope that it will be useful,                     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
+ * GNU Affero General Public License for more details.                             *
+ *                                                                                 *
+ * You should have received a copy of the GNU Affero General Public License        *
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
+ ***********************************************************************************/
+
+package com.projectswg.holocore.resources.support.objects.swg
+
+import com.projectswg.common.encoding.StringType
+import kotlin.reflect.KProperty
+
+class IndirectBaselineDelegate<T>(private val obj: SWGObject, private var value: T, private val page: Int, private val update: Int, private val stringType: StringType? = null) {
+	
+	operator fun getValue(thisRef: Any, property: KProperty<*>): T {
+		return value
+	}
+
+	operator fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+		this.value = value
+		if (stringType != null) {
+			obj.sendDelta(page, update, value, stringType)
+		} else {
+			obj.sendDelta(page, update, value)
+		}
+	}
+	
+}
