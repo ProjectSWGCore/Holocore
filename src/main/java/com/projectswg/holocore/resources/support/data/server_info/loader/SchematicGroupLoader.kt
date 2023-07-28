26e83a448469af0125d371b762acfc906919e183
Ziggy <ziggyswg@outlook.com>
2023-07-28 17:00:04 +0200

    Created data loader for schematic groups, which we will need later to grant draft schematics when relevant skills are granted

---
+++ src/main/java/com/projectswg/holocore/resources/support/data/server_info/loader/SchematicGroupLoader.kt
@@ -0,0 +1,69 @@
+/***********************************************************************************
+ * Copyright (c) 2024 /// Project SWG /// www.projectswg.com                       *
+ *                                                                                 *
+ * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
+ * Our goal is to create an emulator which will provide a server for players to    *
+ * continue playing a game similar to the one they used to play. We are basing     *
+ * it on the final publish of the game prior to end-game events.                   *
+ *                                                                                 *
+ * This file is part of Holocore.                                                  *
+ *                                                                                 *
+ * --------------------------------------------------------------------------------*
+ *                                                                                 *
+ * Holocore is free software: you can redistribute it and/or modify                *
+ * it under the terms of the GNU Affero General Public License as                  *
+ * published by the Free Software Foundation, either version 3 of the              *
+ * License, or (at your option) any later version.                                 *
+ *                                                                                 *
+ * Holocore is distributed in the hope that it will be useful,                     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
+ * GNU Affero General Public License for more details.                             *
+ *                                                                                 *
+ * You should have received a copy of the GNU Affero General Public License        *
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
+ ***********************************************************************************/
+package com.projectswg.holocore.resources.support.data.server_info.loader
+
+import com.projectswg.holocore.resources.support.data.server_info.SdbLoader
+import java.io.File
+
+class SchematicGroupLoader : DataLoader() {
+
+	private val schematicGroupMap = mutableMapOf<String, MutableCollection<String>>()
+
+	/**
+	 * Returns a collection of all schematic names in the given group
+	 * Example: getSchematicsInGroup("craftDroidDamageRepairA") returns a collection of ["object/draft_schematic/droid/droid_damage_repair_kit_a.iff"]
+	 */
+	fun getSchematicsInGroup(groupId: String): Collection<String> {
+		return schematicGroupMap.getOrElse(groupId) { emptyList() }
+	}
+
+	override fun load() {
+		val set = SdbLoader.load(File("serverdata/crafting/schematic_group.sdb"))
+
+		set.use {
+			while (set.next()) {
+				val groupid = set.getText("groupid")
+				val schematicname = set.getText("schematicname")
+				
+				if (groupid != "end") {
+					ensureSchematicGroupExists(groupid)
+					appendSchematicToGroup(groupid, schematicname)
+				}
+			}
+		}
+	}
+
+	private fun appendSchematicToGroup(groupid: String, schematicname: String) {
+		schematicGroupMap[groupid]?.add(schematicname)
+	}
+
+	private fun ensureSchematicGroupExists(groupid: String) {
+		if (!schematicGroupMap.containsKey(groupid)) {
+			schematicGroupMap[groupid] = mutableListOf()
+		}
+	}
+}
---
+++ src/test/java/com/projectswg/holocore/resources/support/data/server_info/loader/SchematicGroupLoaderTest.kt
@@ -0,0 +1,67 @@
+/***********************************************************************************
+ * Copyright (c) 2024 /// Project SWG /// www.projectswg.com                       *
+ *                                                                                 *
+ * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
+ * Our goal is to create an emulator which will provide a server for players to    *
+ * continue playing a game similar to the one they used to play. We are basing     *
+ * it on the final publish of the game prior to end-game events.                   *
+ *                                                                                 *
+ * This file is part of Holocore.                                                  *
+ *                                                                                 *
+ * --------------------------------------------------------------------------------*
+ *                                                                                 *
+ * Holocore is free software: you can redistribute it and/or modify                *
+ * it under the terms of the GNU Affero General Public License as                  *
+ * published by the Free Software Foundation, either version 3 of the              *
+ * License, or (at your option) any later version.                                 *
+ *                                                                                 *
+ * Holocore is distributed in the hope that it will be useful,                     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
+ * GNU Affero General Public License for more details.                             *
+ *                                                                                 *
+ * You should have received a copy of the GNU Affero General Public License        *
+ * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
+ ***********************************************************************************/
+package com.projectswg.holocore.resources.support.data.server_info.loader
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SchematicGroupLoaderTest {
+	@Test
+	fun groupsByGroupIdCorrectly() {
+		val groupId = "craftAdvancedCreatureGroup"
+		val expected = setOf(
+			"object/draft_schematic/bio_engineer/creature/creature_torton.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_kimogila.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_rancor.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_fambaa.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_veermok.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_graul.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_huf_dun.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_malkloc.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_sharnaff.iff",
+			"object/draft_schematic/bio_engineer/creature/creature_woolamander.iff",
+		)
+
+		val schematicsInGroup = ServerData.schematicGroups.getSchematicsInGroup(groupId)
+		
+		assertEquals(expected.size, schematicsInGroup.size)
+		assertTrue(schematicsInGroup.containsAll(expected))
+	}
+
+	@Test
+	fun unknownGroupIdGivesEmptyCollection() {
+		val schematicsInGroup = ServerData.schematicGroups.getSchematicsInGroup("thisGroupDefinitelyDoesNotExist")
+		assertTrue(schematicsInGroup.isEmpty())
+	}
+
+	@Test
+	fun endGroupIdIsIgnored() {
+		// There's a group called "end" in the client info file, but it's not a real group
+		val schematicsInGroup = ServerData.schematicGroups.getSchematicsInGroup("end")
+		assertTrue(schematicsInGroup.isEmpty())
+	}
+}
