94e4893b89a2ab63257bdc596783295c85f9cdff
Undercova <petertigges1977@googlemail.com>
2024-02-04 15:10:38 +0100

    Updated the Test for LS to match the updated crystal values

--- src/test/java/com/projectswg/holocore/resources/support/global/commands/callbacks/LightsaberInventoryTest.kt
+++ src/test/java/com/projectswg/holocore/resources/support/global/commands/callbacks/LightsaberInventoryTest.kt
@@ -74,7 +74,7 @@ class LightsaberInventoryTest : TestRunnerSimulatedWorld() {
 
 		transferItem(player, colorCrystal, lightsaber.lightsaberInventory)
 
-		assertEquals(14, lightsaber.elementalValue)	// The color crystal takes 6% of lightsaber max damage and turns that into the elemental value, rounded down
+		assertEquals(12, lightsaber.elementalValue)	// The color crystal takes 4% of lightsaber max damage and turns that into the elemental value, rounded down
 	}
 
 	@Test
