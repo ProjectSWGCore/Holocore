f3c674472ca001328d88a41919fc16166c8bd319
Ziggy <ziggyswg@outlook.com>
2024-06-05 16:30:00 +0200

    BuffService Java -> Kotlin

--- src/main/java/com/projectswg/holocore/services/gameplay/combat/buffs/BuffService.kt
+++ src/main/java/com/projectswg/holocore/services/gameplay/combat/buffs/BuffService.kt
@@ -24,249 +24,230 @@
  * You should have received a copy of the GNU Affero General Public License        *
  * along with Holocore.  If not, see <http://www.gnu.org/licenses/>.               *
  ***********************************************************************************/
-package com.projectswg.holocore.services.gameplay.combat.buffs;
+package com.projectswg.holocore.services.gameplay.combat.buffs
 
-import com.projectswg.common.data.CRC;
-import com.projectswg.common.network.packets.swg.zone.PlayClientEffectObjectMessage;
-import com.projectswg.holocore.ProjectSWG;
-import com.projectswg.holocore.intents.gameplay.combat.CreatureKilledIntent;
-import com.projectswg.holocore.intents.gameplay.combat.buffs.BuffIntent;
-import com.projectswg.holocore.intents.gameplay.player.experience.skills.SkillModIntent;
-import com.projectswg.holocore.intents.support.global.zone.PlayerEventIntent;
-import com.projectswg.holocore.resources.support.data.server_info.StandardLog;
-import com.projectswg.holocore.resources.support.data.server_info.loader.BuffLoader;
-import com.projectswg.holocore.resources.support.data.server_info.loader.BuffLoader.BuffInfo;
-import com.projectswg.holocore.resources.support.data.server_info.loader.DataLoader;
-import com.projectswg.holocore.resources.support.global.player.Player;
-import com.projectswg.holocore.resources.support.global.player.PlayerEvent;
-import com.projectswg.holocore.resources.support.objects.swg.creature.Buff;
-import com.projectswg.holocore.resources.support.objects.swg.creature.CreatureObject;
-import me.joshlarson.jlcommon.concurrency.ScheduledThreadPool;
-import me.joshlarson.jlcommon.control.IntentHandler;
-import me.joshlarson.jlcommon.control.Service;
-import org.jetbrains.annotations.NotNull;
+import com.projectswg.common.data.CRC
+import com.projectswg.common.network.packets.swg.zone.PlayClientEffectObjectMessage
+import com.projectswg.holocore.ProjectSWG
+import com.projectswg.holocore.intents.gameplay.combat.CreatureKilledIntent
+import com.projectswg.holocore.intents.gameplay.combat.buffs.BuffIntent
+import com.projectswg.holocore.intents.gameplay.player.experience.skills.SkillModIntent
+import com.projectswg.holocore.intents.support.global.zone.PlayerEventIntent
+import com.projectswg.holocore.resources.support.data.server_info.StandardLog
+import com.projectswg.holocore.resources.support.data.server_info.loader.BuffLoader.BuffInfo
+import com.projectswg.holocore.resources.support.data.server_info.loader.DataLoader.Companion.commands
+import com.projectswg.holocore.resources.support.data.server_info.loader.ServerData
+import com.projectswg.holocore.resources.support.global.player.PlayerEvent
+import com.projectswg.holocore.resources.support.objects.swg.creature.Buff
+import com.projectswg.holocore.resources.support.objects.swg.creature.CreatureObject
+import me.joshlarson.jlcommon.concurrency.ScheduledThreadPool
+import me.joshlarson.jlcommon.control.IntentHandler
+import me.joshlarson.jlcommon.control.Service
+import java.util.*
 
-import java.util.*;
+class BuffService : Service() {
+	private val timerCheckThread = ScheduledThreadPool(1, "buff-timer-check")
+	private val callbackMap: MutableMap<String, BuffCallback> = HashMap()
+	private val buffs = ServerData.buffs
 
-public class BuffService extends Service {
-	
-	
-	private final ScheduledThreadPool timerCheckThread;
-	private final Map<String, BuffCallback> callbackMap;
-	private final BuffLoader buffs = DataLoader.Companion.buffs();
+	init {
+		registerCallbacks()
+	}
 
-	public BuffService() {
-		timerCheckThread = new ScheduledThreadPool(1, "buff-timer-check");
-		callbackMap = new HashMap<>();
-		registerCallbacks();
+	private fun registerCallbacks() {
+		callbackMap["removeBurstRun"] = RemoveBurstRunBuffCallback()
 	}
-	
-	private void registerCallbacks() {
-		callbackMap.put("removeBurstRun", new RemoveBurstRunBuffCallback());
+
+	override fun start(): Boolean {
+		timerCheckThread.start()
+		return super.start()
 	}
-	
-	@Override
-	public boolean start() {
-		timerCheckThread.start();
-		return super.start();
+
+	override fun stop(): Boolean {
+		timerCheckThread.stop()
+		return super.stop()
 	}
-	
-	@Override
-	public boolean stop() {
-		timerCheckThread.stop();
-		return super.stop();
-	}
-	
+
 	@IntentHandler
-	private void handleBuffIntent(BuffIntent bi) {
-		String buffName = bi.getBuffName();
-		CRC buffCrc = new CRC(CRC.getCrc(buffName.toLowerCase(Locale.ENGLISH)));
-		CreatureObject buffer = bi.getBuffer();
-		CreatureObject receiver = bi.getReceiver();
+	private fun handleBuffIntent(bi: BuffIntent) {
+		val buffName = bi.buffName
+		val buffCrc = CRC(CRC.getCrc(buffName.lowercase()))
+		val buffer = bi.buffer
+		val receiver = bi.receiver
 
-		if (bi.isRemove()) {
-			removeBuff(receiver, buffCrc);
+		if (bi.isRemove) {
+			removeBuff(receiver, buffCrc)
 		} else {
-			addBuff(receiver, buffCrc, buffer);
+			addBuff(receiver, buffCrc, buffer)
 		}
 	}
-	
+
 	@IntentHandler
-	private void handleCreatureKilledIntent(CreatureKilledIntent cki) {
-		removeAllBuffs(cki.getCorpse());
+	private fun handleCreatureKilledIntent(cki: CreatureKilledIntent) {
+		removeAllBuffs(cki.corpse)
 	}
-	
+
 	@IntentHandler
-	private void handlePlayerEventIntent(PlayerEventIntent intent) {
-		PlayerEvent event = intent.getEvent();
-		
+	private fun handlePlayerEventIntent(intent: PlayerEventIntent) {
+		val event = intent.event
+
 		if (event == PlayerEvent.PE_ZONE_IN_SERVER) {
-			removeExpiredBuffs(intent.getPlayer().getCreatureObject());
+			removeExpiredBuffs(intent.player.creatureObject)
 		}
 	}
-	
-	private void removeExpiredBuffs(CreatureObject creatureObject) {
-		Collection<CRC> crcsForExpiredBuffs = creatureObject.getBuffs().entrySet().stream()
-				.filter(entry -> {
-					Buff buff = entry.getValue();
-					return isBuffExpired(creatureObject, buff);
-				}).map(Map.Entry::getKey)
-				.toList();
 
-		crcsForExpiredBuffs.forEach(buffCrc -> removeBuff(creatureObject, buffCrc));
+	private fun removeExpiredBuffs(creatureObject: CreatureObject) {
+		val crcsForExpiredBuffs = creatureObject.buffs.filter { isBuffExpired(creatureObject, it.value) }.map { it.key }
+
+		crcsForExpiredBuffs.forEach { removeBuff(creatureObject, it) }
 	}
-	
-	private int calculatePlayTime(CreatureObject creature) {
-		if (creature.isPlayer()) {
-			creature.getPlayerObject().updatePlayTime();
-			return creature.getPlayerObject().getPlayTime();
+
+	private fun calculatePlayTime(creature: CreatureObject): Int {
+		if (creature.isPlayer) {
+			creature.playerObject.updatePlayTime()
+			return creature.playerObject.playTime
 		}
-		
+
 		// NPC time is based on the current galactic time because NPCs don't have playTime
-		return (int) ProjectSWG.getGalacticTime();
+		return ProjectSWG.galacticTime.toInt()
 	}
-	
-	private boolean isBuffExpired(CreatureObject creature, Buff buff) {
-		return calculatePlayTime(creature) >= buff.getEndTime();
-	}
-	
-	private boolean isBuffInfinite(BuffInfo buffData) {
-		return buffData.getDuration() < 0;
-	}
-	
-	private void removeAllBuffs(CreatureObject creature) {
-		Set<CRC> buffCrcs = creature.getBuffs().keySet();
-		buffCrcs.forEach(buffCrc -> removeBuff(creature, buffCrc));
-	}
-	
-	private void addBuff(CreatureObject receiver, @NotNull CRC buffCrc, CreatureObject buffer) {
-		BuffInfo buffData = buffs.getBuff(buffCrc);
-		String groupName = buffData.getGroup1();
-		Optional<CRC> optGroupBuffCrc = receiver.getBuffs().keySet().stream()
-				.map(buffCrc1 -> buffs.getBuff(buffCrc1)).filter(buff -> {
-					CRC candidateBuffCrc = buff.getCrc();
-					BuffInfo candidateBuff = buffs.getBuff(candidateBuffCrc);
-					String candidateBuffGroup1Name = candidateBuff.getGroup1();
 
-					return groupName.equals(candidateBuffGroup1Name);
-				})
-				.map(BuffInfo::getCrc)
-				.findAny();
-		
-		if (optGroupBuffCrc.isPresent()) {
-			CRC groupBuffCrc = optGroupBuffCrc.get();
-			boolean sameBuff = groupBuffCrc.equals(buffCrc);
-			
+	private fun isBuffExpired(creature: CreatureObject, buff: Buff): Boolean {
+		return calculatePlayTime(creature) >= buff.endTime
+	}
+
+	private fun isBuffInfinite(buffData: BuffInfo): Boolean {
+		return buffData.duration < 0
+	}
+
+	private fun removeAllBuffs(creature: CreatureObject) {
+		val buffCrcs = creature.buffs.keys
+		buffCrcs.forEach { removeBuff(creature, it) }
+	}
+
+	private fun addBuff(receiver: CreatureObject, buffCrc: CRC, buffer: CreatureObject) {
+		val buffData = buffs.getBuff(buffCrc)
+		if (buffData == null) {
+			StandardLog.onPlayerError(this, buffer, "buff by name '%s' could not be looked up", buffCrc.string)
+			return
+		}
+		val groupName = buffData.group1
+		val existingGroup1Buff = receiver.buffs.keys.asSequence().map { buffs.getBuff(it) }.filterNotNull().filter {
+			val candidateBuffGroup1Name = it.group1
+			groupName == candidateBuffGroup1Name
+		}.firstOrNull()
+
+		if (existingGroup1Buff != null) {
+			val existingGroup1BuffCrc = existingGroup1Buff.crc
+			val sameBuff = existingGroup1BuffCrc == buffCrc
+
 			if (sameBuff) {
 				if (isBuffInfinite(buffData)) {
-					removeBuff(receiver, buffCrc);
+					removeBuff(receiver, buffCrc)
 				} else {
 					// Reset timer
-					removeBuff(receiver, buffCrc);
-					applyBuff(receiver, buffer, buffData);
+					removeBuff(receiver, buffCrc)
+					applyBuff(receiver, buffer, buffData)
 				}
 			} else {
-				BuffInfo oldBuff = buffs.getBuff(groupBuffCrc);
-				if (buffData.getPriority() >= oldBuff.getPriority()) {
-					removeBuff(receiver, groupBuffCrc);
-					applyBuff(receiver, buffer, buffData);
+				if (buffData.priority >= existingGroup1Buff.priority) {
+					removeBuff(receiver, existingGroup1BuffCrc)
+					applyBuff(receiver, buffer, buffData)
 				}
 			}
 		} else {
-			applyBuff(receiver, buffer, buffData);
+			applyBuff(receiver, buffer, buffData)
 		}
 	}
-	
-	private void removeBuff(CreatureObject creature, @NotNull CRC buffCrc) {
-		if (!creature.getBuffs().containsKey(buffCrc))
-			return; // Obique: Used to be an assertion, however if a service sends the removal after it expires it would assert - so I just removed it.
 
-		BuffInfo buffData = buffs.getBuff(buffCrc);
+	private fun removeBuff(creature: CreatureObject, buffCrc: CRC) {
+		if (!creature.buffs.containsKey(buffCrc)) return  // Obique: Used to be an assertion, however if a service sends the removal after it expires it would assert - so I just removed it.
+
+		val buffData = buffs.getBuff(buffCrc)
 		if (buffData == null) {
-			StandardLog.onPlayerError(this, creature, "unable to remove buff '%s' as it could not be looked up", buffCrc.getString());
-			return;
-		}
-		Buff removedBuff = creature.removeBuff(buffCrc);
-		Objects.requireNonNull(removedBuff, "Buff must exist if being removed");
-		StandardLog.onPlayerTrace(this, creature, "buff '%s' was removed", buffCrc.getString());
-
-		checkBuffEffects(buffData, creature, false);
-		checkCallback(buffData, creature);
-	}
-	
-	private void applyBuff(CreatureObject receiver, CreatureObject buffer, BuffInfo buffData) {
-		int applyTime = calculatePlayTime(receiver);
-		int buffDuration = (int) buffData.getDuration();
-		int endTime = applyTime + buffDuration;
-		
-		{
-			Player bufferPlayer = buffer.getOwner();
-			String bufferUsername = bufferPlayer == null ? "NULL" : bufferPlayer.getUsername();
-			StandardLog.onPlayerTrace(this, receiver, "received buff '%s' from %s/%s; applyTime: %d, buffDuration: %d", buffData.getName(), bufferUsername, buffer.getObjectName(), applyTime, buffDuration);
+			StandardLog.onPlayerError(this, creature, "unable to remove buff '%s' as it could not be looked up", buffCrc.string)
+			return
 		}
 
-		checkBuffEffects(buffData, receiver, true);
-		receiver.addBuff(buffData.getCrc(), new Buff(endTime));
+		val removedBuff = creature.removeBuff(buffCrc)
+		Objects.requireNonNull(removedBuff, "Buff must exist if being removed")
+		StandardLog.onPlayerTrace(this, creature, "buff '%s' was removed", buffCrc.string)
 
-		sendParticleEffect(buffData.getParticle(), receiver, "");
-
-		scheduleBuffExpirationCheck(receiver, buffData);
+		checkBuffEffects(buffData, creature, false)
+		checkCallback(buffData, creature)
 	}
 
-	private void scheduleBuffExpirationCheck(CreatureObject receiver, BuffInfo buffData) {
-		timerCheckThread.execute(1000L, () -> {
-			Buff buff = receiver.getBuffs().get(buffData.getCrc());
-			if (buff == null) {
-				return;
-			}
+	private fun applyBuff(receiver: CreatureObject, buffer: CreatureObject, buffData: BuffInfo) {
+		val applyTime = calculatePlayTime(receiver)
+		val buffDuration = buffData.duration.toInt()
+		val endTime = applyTime + buffDuration
+
+		val bufferPlayer = buffer.owner
+		val bufferUsername = if (bufferPlayer == null) "NULL" else bufferPlayer.username
+		StandardLog.onPlayerTrace(this, receiver, "received buff '%s' from %s/%s; applyTime: %d, buffDuration: %d", buffData.name, bufferUsername, buffer.objectName, applyTime, buffDuration)
+
+		checkBuffEffects(buffData, receiver, true)
+		receiver.addBuff(buffData.crc, Buff(endTime))
+
+		sendParticleEffect(buffData.particle, receiver, "")
+
+		scheduleBuffExpirationCheck(receiver, buffData)
+	}
+
+	private fun scheduleBuffExpirationCheck(receiver: CreatureObject, buffData: BuffInfo) {
+		timerCheckThread.execute(1000L) {
+			val buff = receiver.buffs[buffData.crc] ?: return@execute
 			if (isBuffExpired(receiver, buff)) {
-				removeBuff(receiver, buffData.getCrc());
+				removeBuff(receiver, buffData.crc)
 			} else {
-				scheduleBuffExpirationCheck(receiver, buffData);
+				scheduleBuffExpirationCheck(receiver, buffData)
 			}
-		});
-	}
-
-	private void sendParticleEffect(String effectFileName, CreatureObject receiver, String hardPoint) {
-		if (effectFileName != null && !effectFileName.isEmpty()) {
-			receiver.sendObservers(new PlayClientEffectObjectMessage(effectFileName, hardPoint, receiver.getObjectId(), ""));
 		}
 	}
-	
-	private void checkCallback(BuffInfo buffData, CreatureObject creature) {
-		String callback = buffData.getCallback();
+
+	private fun sendParticleEffect(effectFileName: String?, receiver: CreatureObject, hardPoint: String) {
+		if (!effectFileName.isNullOrEmpty()) {
+			receiver.sendObservers(PlayClientEffectObjectMessage(effectFileName, hardPoint, receiver.objectId, ""))
+		}
+	}
+
+	private fun checkCallback(buffData: BuffInfo, creature: CreatureObject) {
+		val callback = buffData.callback
+
+		callbackMap[callback]?.execute(creature)
+	}
+
+	private fun checkBuffEffects(buffData: BuffInfo, creature: CreatureObject, add: Boolean) {
+		for (i in 0..4) checkBuffEffect(creature, buffData.getEffectName(i), buffData.getEffectValue(i), add)
+	}
+
+	private fun checkBuffEffect(creature: CreatureObject, effectName: String?, effectValue: Double, add: Boolean) {
+		if (effectName.isNullOrEmpty()) {
+			return
+		}
+
+		val command = commands().isCommand(effectName) && effectValue == 1.0
 		
-		if (callbackMap.containsKey(callback)) {
-			BuffCallback buffCallback = callbackMap.get(callback);
-			buffCallback.execute(creature);
-		}
-	}
-	
-	private void checkBuffEffects(BuffInfo buffData, CreatureObject creature, boolean add) {
-		for (int i = 0; i < 5; i++)
-			checkBuffEffect(creature, buffData.getEffectName(i), buffData.getEffectValue(i), add);
-	}
-	
-	private void checkBuffEffect(CreatureObject creature, String effectName, double effectValue, boolean add) {
-		if (effectName != null &&  !effectName.isEmpty()) {
-			if (DataLoader.Companion.commands().isCommand(effectName) && effectValue == 1.0) {
-				// This effect is an ability
-				if (add) {
-					// Buff is being added. Grant the ability.
-					creature.addCommand(effectName);
-				} else {
-					// Buff is being removed. Remove the ability.
-					creature.removeCommand(effectName);
-				}
-			} else {
-				// This effect is a skill mod
-				if (add) {
-					new SkillModIntent(effectName, 0, (int) effectValue, creature).broadcast();
-				} else {
-					new SkillModIntent(effectName, 0, (int) -effectValue, creature).broadcast();
-				}
-			}
+		if (command) {
+			checkCommand(add, creature, effectName)
+		} else {
+			checkSkillMod(add, effectName, effectValue, creature)
 		}
 	}
 
+	private fun checkSkillMod(add: Boolean, effectName: String, effectValue: Double, creature: CreatureObject) {
+		if (add) {
+			SkillModIntent(effectName, 0, effectValue.toInt(), creature).broadcast()
+		} else {
+			SkillModIntent(effectName, 0, -effectValue.toInt(), creature).broadcast()
+		}
+	}
+
+	private fun checkCommand(add: Boolean, creature: CreatureObject, effectName: String) {
+		if (add) {
+			creature.addCommand(effectName)
+		} else {
+			creature.removeCommand(effectName)
+		}
+	}
 }
