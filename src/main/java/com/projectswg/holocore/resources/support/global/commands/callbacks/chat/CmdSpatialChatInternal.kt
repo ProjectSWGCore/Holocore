8313d8a6d7c6afc8f8869121b6744d0cf54a66ef
Josh Larson <joshlarson2015@gmail.com>
2024-10-10 16:43:02 -0500

    Fixed unintentional log in previous commit

--- src/main/java/com/projectswg/holocore/resources/support/global/commands/callbacks/chat/CmdSpatialChatInternal.kt
+++ src/main/java/com/projectswg/holocore/resources/support/global/commands/callbacks/chat/CmdSpatialChatInternal.kt
@@ -29,12 +29,10 @@ import com.projectswg.holocore.intents.support.global.chat.SpatialChatIntent
 import com.projectswg.holocore.resources.support.global.commands.ICmdCallback
 import com.projectswg.holocore.resources.support.global.player.Player
 import com.projectswg.holocore.resources.support.objects.swg.SWGObject
-import me.joshlarson.jlcommon.log.Log
 
 class CmdSpatialChatInternal : ICmdCallback {
 	override fun execute(player: Player, target: SWGObject?, args: String) {
 		val cmdArgs = args.split(" ".toRegex(), limit = 6).toTypedArray()
-		Log.d("Spatial Chat: '$args'")
 
 		SpatialChatIntent(player, cmdArgs[0].toLong(), cmdArgs[1].toInt(), cmdArgs[5], cmdArgs[2].toInt(), cmdArgs[4].toInt()).broadcast()
 	}
