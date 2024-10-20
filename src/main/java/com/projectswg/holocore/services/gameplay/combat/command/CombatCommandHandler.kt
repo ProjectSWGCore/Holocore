4bb5818d7dbb4faf2c6ead467d1bbed3c6952c0e
Josh Larson <joshlarson2015@gmail.com>
2024-10-20 17:21:18 -0500

    Implemented kipUp, dive, and roll shots

--- src/main/java/com/projectswg/holocore/services/gameplay/combat/command/CombatCommandHandler.kt
+++ src/main/java/com/projectswg/holocore/services/gameplay/combat/command/CombatCommandHandler.kt
@@ -1,11 +1,10 @@
 /***********************************************************************************
  * Copyright (c) 2024 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
- * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * ProjectSWG is an emulation project for Star Wars Galaxies founded on            *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
- * Our goal is to create an emulator which will provide a server for players to    *
- * continue playing a game similar to the one they used to play. We are basing     *
- * it on the final publish of the game prior to end-game events.                   *
+ * Our goal is to create one or more emulators which will provide servers for      *
+ * players to continue playing a game similar to the one they used to play.        *
  *                                                                                 *
  * This file is part of Holocore.                                                  *
  *                                                                                 *
@@ -29,6 +28,7 @@ package com.projectswg.holocore.services.gameplay.combat.command
 
 import com.projectswg.holocore.resources.gameplay.combat.CombatStatus
 import com.projectswg.common.data.combat.HitType
+import com.projectswg.common.data.encodables.tangible.Posture
 import com.projectswg.holocore.resources.support.global.commands.CombatCommand
 import com.projectswg.holocore.resources.support.global.commands.Command
 import com.projectswg.holocore.resources.support.objects.swg.SWGObject
@@ -39,7 +39,7 @@ import java.util.*
 class CombatCommandHandler(toHitDie: Die, knockdownDie: Die, woundDie: Die) {
 	
 	private val hitTypeMap: MutableMap<HitType, CombatCommandHitType> = EnumMap(HitType::class.java)
-
+	
 	init {
 		val combatCommandAttack = CombatCommandAttack(toHitDie, knockdownDie, woundDie)
 		hitTypeMap[HitType.ATTACK] = combatCommandAttack
@@ -51,14 +51,12 @@ class CombatCommandHandler(toHitDie: Die, knockdownDie: Die, woundDie: Die) {
 	}
 	
 	fun start(): Boolean {
-		for (hitType in hitTypeMap.values)
-			hitType.initialize()
+		for (hitType in hitTypeMap.values) hitType.initialize()
 		return true
 	}
 	
 	fun stop(): Boolean {
-		for (hitType in hitTypeMap.values)
-			hitType.terminate()
+		for (hitType in hitTypeMap.values) hitType.terminate()
 		return true
 	}
 	
@@ -70,14 +68,14 @@ class CombatCommandHandler(toHitDie: Die, knockdownDie: Die, woundDie: Die) {
 		val mindCost = (combatCommand.mindCost * specialAttackCost / 100).toInt()
 		val forceCost = (combatCommand.forceCost) + (equippedWeapon.forcePowerCost * combatCommand.forceCostModifier)
 		val playerObject = source.playerObject
-
+		
 		if (healthCost > source.health || actionCost > source.action || mindCost > source.mind || (playerObject != null && forceCost > playerObject.forcePower)) {
 			return CombatStatus.TOO_TIRED
 		}
 		
 		val hitType = hitTypeMap[combatCommand.hitType]
 		val combatStatus = hitType?.handle(source, target, command, combatCommand, arguments) ?: CombatStatus.UNKNOWN
-
+		
 		if (combatStatus == CombatStatus.SUCCESS) {
 			source.modifyHealth(-healthCost)
 			source.modifyAction(-actionCost)
@@ -86,9 +84,16 @@ class CombatCommandHandler(toHitDie: Die, knockdownDie: Die, woundDie: Die) {
 			if (playerObject != null) {
 				playerObject.forcePower = (playerObject.forcePower - forceCost).toInt()
 			}
+			
+			// Special case commands that involve a posture change
+			when (command.name) {
+				"kipupshot" -> source.posture = Posture.UPRIGHT
+				"rollshot"  -> source.posture = Posture.CROUCHED
+				"diveshot"  -> source.posture = Posture.PRONE
+			}
 		}
-
+		
 		return combatStatus
 	}
-
+	
 }
