5759a5549a1fd9bdfd3ef1a8048d197fe780e46b
Josh Larson <joshlarson2015@gmail.com>
2024-11-30 18:51:47 -0600

    Fixed #1454 - resource object name should be resource class

--- src/main/java/com/projectswg/holocore/resources/support/data/server_info/loader/ServerData.kt
+++ src/main/java/com/projectswg/holocore/resources/support/data/server_info/loader/ServerData.kt
@@ -1,11 +1,10 @@
 /***********************************************************************************
  * Copyright (c) 2024 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
- * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * ProjectSWG is an emulation project for Star Wars Galaxies founded on            *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
- * Our goal is to create an emulator which will provide a server for players to    *
- * continue playing a game similar to the one they used to play. We are basing     *
- * it on the final publish of the game prior to end-game events.                   *
+ * Our goal is to create one or more emulators which will provide servers for      *
+ * players to continue playing a game similar to the one they used to play.        *
  *                                                                                 *
  * This file is part of Holocore.                                                  *
  *                                                                                 *
@@ -84,6 +83,7 @@ object ServerData {
 	val slotArrangements	by SoftDataLoaderDelegate(::SlotArrangementLoader)
 	val planetMapCategories	by SoftDataLoaderDelegate(::PlanetMapCategoryLoader)
 	val zoneInsertions		by SoftDataLoaderDelegate(::TerrainZoneInsertionLoader)
+	val strings				by SoftDataLoaderDelegate(::StringLoader)
 	val terrains            by SoftDataLoaderDelegate(::TerrainHeightLoader)
 	
 	val elevators           by SoftDataLoaderDelegate(::ElevatorLoader)
