c4b03de0e33b4aa9120f376716eb75c320bec000
Josh Larson <joshlarson2015@gmail.com>
2025-01-14 20:36:09 -0600

    Fixed TravelHelper tests

--- src/test/java/com/projectswg/holocore/test/runners/TestRunnerNoIntents.java
+++ src/test/java/com/projectswg/holocore/test/runners/TestRunnerNoIntents.java
@@ -1,11 +1,10 @@
 /***********************************************************************************
- * Copyright (c) 2018 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2025 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
- * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * ProjectSWG is an emulation project for Star Wars Galaxies founded on            *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
- * Our goal is to create an emulator which will provide a server for players to    *
- * continue playing a game similar to the one they used to play. We are basing     *
- * it on the final publish of the game prior to end-game events.                   *
+ * Our goal is to create one or more emulators which will provide servers for      *
+ * players to continue playing a game similar to the one they used to play.        *
  *                                                                                 *
  * This file is part of Holocore.                                                  *
  *                                                                                 *
@@ -27,6 +26,7 @@
 
 package com.projectswg.holocore.test.runners;
 
+import com.projectswg.holocore.utilities.HolocoreCoroutine;
 import me.joshlarson.jlcommon.control.IntentManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,15 +34,19 @@ import org.junit.jupiter.api.BeforeEach;
 public abstract class TestRunnerNoIntents extends TestRunner {
 	
 	private static final IntentManager INTENT_MANAGER = new IntentManager(0);
+	private HolocoreCoroutine holocoreCoroutine = null;
 	
 	@BeforeEach
 	public void initializeIntents() {
 		IntentManager.setInstance(INTENT_MANAGER);
+		holocoreCoroutine = new HolocoreCoroutine();
+		HolocoreCoroutine.Companion.getINSTANCE().set(holocoreCoroutine);
 	}
 	
 	@AfterEach
 	public void terminateIntents() {
 		IntentManager.setInstance(null);
+		holocoreCoroutine.close();
 	}
 	
 }
