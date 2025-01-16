e72c5103acdf3f531ed221c7745280e264dbe73f
Josh Larson <joshlarson2015@gmail.com>
2025-01-16 14:56:52 -0600

    Improved reliability of LootTest

--- src/test/java/com/projectswg/holocore/services/gameplay/combat/loot/LootTest.kt
+++ src/test/java/com/projectswg/holocore/services/gameplay/combat/loot/LootTest.kt
@@ -1,11 +1,10 @@
 /***********************************************************************************
- * Copyright (c) 2024 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2025 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
- * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * ProjectSWG is an emulation project for Star Wars Galaxies founded on            *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
- * Our goal is to create an emulator which will provide a server for players to    *
- * continue playing a game similar to the one they used to play. We are basing     *
- * it on the final publish of the game prior to end-game events.                   *
+ * Our goal is to create one or more emulators which will provide servers for      *
+ * players to continue playing a game similar to the one they used to play.        *
  *                                                                                 *
  * This file is part of Holocore.                                                  *
  *                                                                                 *
@@ -29,6 +28,7 @@ package com.projectswg.holocore.services.gameplay.combat.loot
 import com.projectswg.holocore.headless.HeadlessSWGClient.Companion.createZonedInCharacter
 import com.projectswg.holocore.headless.attack
 import com.projectswg.holocore.headless.loot
+import com.projectswg.holocore.headless.waitUntilAwareOf
 import com.projectswg.holocore.test.runners.AcceptanceTest
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -41,6 +41,8 @@ class LootTest : AcceptanceTest() {
 		val zonedInCharacter = createZonedInCharacter(user.username, user.password, "tester")
 		val npc = spawnNPC("creature_kreetle_swarmling", zonedInCharacter.player.creatureObject.location)
 		npc.health = 1
+		
+		zonedInCharacter.waitUntilAwareOf(npc)
 		zonedInCharacter.attack(npc)
 		
 		val availableItems = zonedInCharacter.loot(npc)
