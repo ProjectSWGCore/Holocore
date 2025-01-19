0998a3cb3582058b396f1d8db6d87aa099cd61e9
Josh Larson <joshlarson2015@gmail.com>
2025-01-18 19:23:48 -0600

    Further improved the consistency of the loot test

--- src/test/java/com/projectswg/holocore/headless/loot.kt
+++ src/test/java/com/projectswg/holocore/headless/loot.kt
@@ -1,11 +1,10 @@
 /***********************************************************************************
- * Copyright (c) 2023 /// Project SWG /// www.projectswg.com                       *
+ * Copyright (c) 2025 /// Project SWG /// www.projectswg.com                       *
  *                                                                                 *
- * ProjectSWG is the first NGE emulator for Star Wars Galaxies founded on          *
+ * ProjectSWG is an emulation project for Star Wars Galaxies founded on            *
  * July 7th, 2011 after SOE announced the official shutdown of Star Wars Galaxies. *
- * Our goal is to create an emulator which will provide a server for players to    *
- * continue playing a game similar to the one they used to play. We are basing     *
- * it on the final publish of the game prior to end-game events.                   *
+ * Our goal is to create one or more emulators which will provide servers for      *
+ * players to continue playing a game similar to the one they used to play.        *
  *                                                                                 *
  * This file is part of Holocore.                                                  *
  *                                                                                 *
@@ -37,6 +36,14 @@ import com.projectswg.holocore.resources.support.objects.swg.creature.CreatureOb
  * @return the objects that can be looted from the target
  */
 fun ZonedInCharacter.loot(target: CreatureObject): Collection<SWGObject> {
+	for (i in 0 until 50) {
+		val inventory = target.getSlottedObject("inventory")
+		if (inventory != null) {
+			waitUntilAwareOf(inventory)
+			break
+		}
+		Thread.sleep(20)
+	}
 	sendPacket(player, ObjectMenuSelect(target.objectId, RadialItem.LOOT.type.toShort()))
 	player.waitForNextPacket(ClientOpenContainerMessage::class.java) ?: throw IllegalStateException("NPC inventory did not open!")
 	return target.inventory.childObjects
